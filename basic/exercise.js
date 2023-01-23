f6208c91fa0d3ea0331dcfe9770f8e51245bc185
akashkhot12 <akashkhot12.com>
2023-01-23 12:59:04 +0530

    day4

--- basic/exercise.js
+++ basic/exercise.js
@@ -1,8 +1,42 @@
+let btn = document.getElementsByTagName("button");
+// let para= document.getElementById("para");
+let ptg = document.getElementsByTagName("p");
 
-function myFunction() {
-  let ak = prompt('What  is your name ?')
-  document.write("Welcome " +ak+"....!");
-  // history.back();
-  
 
+
+// btn.addEventListener("click",function(){
+//       para.style.color="red";
+//       // para.style.color="green";
+// })
+
+
+function clickMe(){
+  if(ptg.style.display === "none")
+  {
+    ptg.style.display= "block";
+    btn.innerHTML = "hide"
+
+  }
+  else{
+    ptg.style.display = "none"
+    btn.innerHTML = "show"
+  }
 }
+
+function tag(){
+  // ptg[i].style.border = ""
+  
+   for (let i = 0;i < ptg.length; i++){
+      if(ptg[i].style.border === "2px solid green"){
+        ptg[i].style.border = "none"
+      }
+      else{
+        ptg[i].style.border = "2px solid green"
+      }
+   }
+}
+
+
+
+
+
