f6e1e8a31d11f85e2d56fbf7a882c78295f7d9f5
Akash khot <akashkhot03@gmail.com>
2023-01-31 11:04:19 +0530

    second largest number in array elements

--- array/max_value_elemets.js
+++ array/max_value_elemets.js
@@ -1,13 +1,16 @@
 let a=[1,2,3,4,40,55,101,-1]
 let max=a[0];
+let max_sec=a[0];
 for(let i=0;i<a.length;i++)
 {
     if(max<a[i])
     {
         max=a[i];
+        max_sec=a[i]
+       
     }
 }
-console.log(max);
+console.log(max_sec);
 
 
 
---
+++ array/second_large.js
@@ -0,0 +1,42 @@
+// function getSecondLargest(nums) {
+//     var flarge = 0;
+//     var slarge = 0;
+//     for (var i = 0; i < nums.length; i++) { 
+//             if (flarge < nums[i]) {
+//                 slarge = flarge;
+//                 flarge = nums[i];
+//             } else if (nums[i] > slarge) { 
+//                 slarge = nums[i]
+
+//             }
+//         }
+//         return slarge; 
+// }
+// console.log(getSecondLargest([77,99,99.5,65,43,96,95])
+// );
+
+let a =[1,2,3,4,10,5,9]
+let max=0;
+let sd_max=0;
+for(let i=0;i<a.length;i++)
+{
+    if (max<a[i])
+     {
+        sd_max=max;
+        max=a[i];        
+    }
+    else(a[i]>sd_max)
+    {
+        sd_max=a[i];
+    }
+}
+console.log(sd_max);
+
+
+
+
+
+
+
+
+
