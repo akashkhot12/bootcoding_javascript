bbff7dbf93460e9383be332a14e2025693bd4023
Akash khot <akashkhot03@gmail.com>
2023-01-31 13:20:29 +0530

    age calculator

---
+++ array/age_cal.js
@@ -0,0 +1,8 @@
+function yourAGe(age){
+    let total_age = 90-age;
+    let days = total_age*365;
+    let weeks = total_age*52;
+    let months = total_age * 12;
+    console.log("your rem days is "+days + " , your rem weeks is "+weeks+ " , your rem months is "+months);
+}
+yourAGe(24);
