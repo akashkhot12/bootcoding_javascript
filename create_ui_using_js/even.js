9a5024c98b37232d2158aa5edc96e5fe6fb06c26
Akash khot <akashkhot03@gmail.com>
2023-02-07 12:16:44 +0530

    cheack the value

---
+++ create_ui_using_js/even.js
@@ -0,0 +1,25 @@
+function even() {
+    
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        console.log(arr[i]);
+    }
+}
+}
+
+function odd(){
+
+    for(let i=0;i<arr.length;i++){
+        if (arry[i]%2!=0) {
+            console.log(arr[i]);
+        }
+    }
+}
+
+function Palindrome () {
+    for(let i=0;i<arr.length;i++){
+        if (arry[i]%2!=0) {
+            console.log(arr[i]);
+        }
+    }
+}
