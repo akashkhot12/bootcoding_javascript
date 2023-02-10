132c8283da4cd05a17b90f9b0e26deabb5a4feab
Akash khot <akashkhot03@gmail.com>
2023-02-10 15:09:46 +0530

    calculator

---
+++ taske and exercise/calcu.js
@@ -0,0 +1,12 @@
+function PointCalculator(win,draw,loss)
+{
+  
+    win=win*3;
+    draw=draw*1;
+    loss=loss*0;
+    let gamePoint= win+draw+loss;
+    return gamePoint;
+}
+
+let gamePoint=PointCalculator(3,4,2);
+console.log(gamePoint);
---
+++ taske and exercise/chekingTwoArguments.js
@@ -0,0 +1,16 @@
+function checking(a,b){
+    let c = a+b;
+    if(a==10 ||b==10){
+        return true;
+    }
+    else if(c = 10)
+    {
+        return true;
+    }
+    else{
+        return false;
+
+    }
+}
+ result = checking(10,5)
+console.log(result);
