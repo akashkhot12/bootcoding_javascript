cb2b525476b7da2e448f1a3845a13a757cb6dcca
Akash khot <akashkhot03@gmail.com>
2023-02-10 14:41:20 +0530

    edited

---
+++ taske and exercise/areaOfTriangle.js
@@ -0,0 +1,6 @@
+function areaOfTriangle(height,base){
+    let result=(base*height)/2;
+    return result;
+}
+result=areaOfTriangle(10,10);
+console.log(result);
---
+++ taske and exercise/stringIsEqualOrNot.js
@@ -0,0 +1,13 @@
+function equality(a,b){
+    a = a.split(',');
+    b = b.split(',')
+    // if(usingSplit==usingSplit1){
+    //     return true;
+    // }
+    // else{
+    //     return false;
+    // }
+    console.log(a,b);
+}
+
+let compare = equality('abc','xyz');
