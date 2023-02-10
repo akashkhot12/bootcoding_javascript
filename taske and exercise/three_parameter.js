b90e6435e8fe471aa03cbc77b1e529fc344ddc35
Akash khot <akashkhot03@gmail.com>
2023-02-10 13:17:10 +0530

    using three parameter in js file

---
+++ taske and exercise/three_parameter.js
@@ -0,0 +1,15 @@
+function three_para(prob,price,pay){
+
+    
+    if ((prob * price) > pay) {
+       
+        return true
+    }
+    else{
+      
+        return false;
+    }
+
+}
+let result =three_para(1,80,9)
+console.log(result);
