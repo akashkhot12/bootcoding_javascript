d7409d0d1b0029d78867030025d5e9fb36acc414
Akash khot <akashkhot03@gmail.com>
2023-02-10 13:22:05 +0530

    sum of string in postive value

--- taske and exercise/sumofstringpositive.js
+++ taske and exercise/sumofstringpositive.js
@@ -1,12 +1,12 @@
-function count() {
-  let arr = [1, 2, 3, 4, -5];
+// let arr = [1, 2, 3, 4, -5];
+function count(a) {
   let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i]>= 0) {
-      sum = sum + arr[i];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i]>= 0) {
+      sum = sum + a[i];
     }
 }
 return sum;
 }
 
-console.log(count());
+console.log(count([1, 2, 3, 4, -5]));
