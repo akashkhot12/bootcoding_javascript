694d62742e9e26eefd3ba335c32accd69ca1214c
Akash khot <akashkhot03@gmail.com>
2023-02-11 13:47:31 +0530

    aading message

--- taske and exercise/calculateTheVehicalDistance.js
+++ taske and exercise/calculateTheVehicalDistance.js
@@ -2,13 +2,15 @@ function distance(km){
   let  dist=10;
   let calc=dist*km;
   if(calc<=100){
-    console.log("you have to pay 100 fuel");
+    // console.log("you have to pay 100 fuel");
+    return "you have to pay 100 fuel";
   }
   else if(calc>100){
-    console.log("you have to pay "+calc+".");
+    // console.log("you have to pay "+calc+".");
+    return "you have to pay "+calc+"."
   }
   else{
-    console.log("plese enter integer number !");
+    return "you have not proper value "
   }
 }
 
