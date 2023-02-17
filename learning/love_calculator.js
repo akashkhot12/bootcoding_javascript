d0c732a64de0d1cff28e87e0f3283bad8cf30517
Akash khot <akashkhot03@gmail.com>
2023-02-17 11:11:28 +0530

    love calculator

--- learning/love_calculator.js
+++ learning/love_calculator.js
@@ -1,7 +1,22 @@
-prompt("what is your name ? ");
-prompt("what is their name ?");
+// var name= prompt("what is your name ? ");
+// var their_name = prompt("what is their name ?");
 
-var loveScore=Math.random()*100;
-loveScore = Math.floor()+1;
+// var loveScore=Math.random()*100;
+// loveScore = Math.floor()+1;
 
-alert("your Love Score is "+loveScore+"%");
+// if (loveScore>70) {
+//    var alert= alert("Amazing ! your Love Score is "+loveScore+"% your love is true");
+// }
+// else{
+//    var alert1 =  alert("your Love Score is "+loveScore+"%");
+// }
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Who are you?', name => {
+    console.log(`Hey there ${name}!`);
+    readline.close();
+  });
