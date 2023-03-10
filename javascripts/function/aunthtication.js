944bfac46da50eea410c26f3c05a8bb4aac8d631
Akash khot <akashkhot03@gmail.com>
2023-03-10 13:38:38 +0530

    user authentication

---
+++ javascripts/function/aunthtication.js
@@ -0,0 +1,11 @@
+var authentication = true;
+
+// if(authentication){
+//     console.log("log in");
+    
+// }
+// else{
+//     console.log("sign in");
+// }
+
+authentication ? console.log("sign in") : console.log("log in");
