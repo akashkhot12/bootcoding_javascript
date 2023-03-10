96bf8b03c97f7046a1bfc9e6a802f8d9acd60002
Akash khot <akashkhot03@gmail.com>
2023-03-10 12:00:42 +0530

    user sign up process

---
+++ javascripts/function/userSignUp.js
@@ -0,0 +1,20 @@
+const uid = "abc123";
+
+var fullName = "akash khot";
+var email = "Akashkhot03@mail.com"
+var password = "12345";
+var ConfirmPassword = "12345";
+var courseCount=0;
+var isLoggedInFromGoogle = false;
+
+// console.log(uid);
+// console.log("full Name is: ", fullName);
+// console.log("email");
+
+console.log(
+    `
+    with Unique ID : ${uid}
+    user is : ${fullName} 
+    and his email is : ${email} 
+    and uses the password :${password} `
+);
