a1e3fa7b6f586baef691443fce2cfc19f4fe4322
Akash khot <akashkhot03@gmail.com>
2023-03-10 12:51:34 +0530

    discount formula

---
+++ javascripts/function/discount.js
@@ -0,0 +1,9 @@
+var sellingPrice = 199;
+var listingPrice = 1000;
+var discountPercent = ((listingPrice - sellingPrice)/listingPrice)*100;
+
+// console.log(discountPercent);
+
+displayDiscountPercentage= Math.round(discountPercent);
+
+console.log(displayDiscountPercentage + "% off");
