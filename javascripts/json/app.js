084b3867008b5bbef5d40de280dfbaf5662d394c
Akash khot <akashkhot03@gmail.com>
2023-03-11 13:03:44 +0530

    json

---
+++ javascripts/json/app.js
@@ -0,0 +1,13 @@
+ 
+  var student=
+    {
+    name:"akash",
+    age : 25,
+    pincode : 441904
+
+   }
+
+   jdata=JSON.stringify(student);
+   console.log(jdata);
+  
+// tranfer object data into a json formate
