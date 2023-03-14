db857a87c2829fe5e89112ec67833131b417806f
Akash khot <akashkhot03@gmail.com>
2023-03-14 14:42:42 +0530

    api genrartor

--- javascripts/json/github search bar/index.js
+++ javascripts/json/github search bar/index.js
@@ -1,6 +1,6 @@
 const APIURL = 'https://api.github.com/users/'
 const main = document.getElementById('main')
-const form = document.getElementById('form')
+const form = document.getElementById('form') 
 const search = document.getElementById('search')
 async function getUser(username) {
     try {
