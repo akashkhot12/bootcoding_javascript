9bba451aa23bb6f7c5ecc980e162cc86777e9b4a
Akash khot <akashkhot03@gmail.com>
2023-03-16 12:47:09 +0530

    using sorting numeric value

--- javascripts/json/profile api generator/event.js
+++ javascripts/json/profile api generator/event.js
@@ -64,17 +64,16 @@
 
 // Sorting Numeric values
 // As you can see in the example below, 100 came first after sorted in ascending order. Sort converts items to string , since '100' and other numbers compared, 1 which the beginning of the string '100' became the smallest. To avoid this, we use a compare call back function inside the sort method, which return a negative, zero or positive.
+// const numbers = [9.81, 3.14, 100, 37]
+// // Using sort method to sort number items provide a wrong result. see below
+// console.log(numbers.sort()) //[100, 3.14, 37, 9.81]
+// numbers.sort(function (a, b) {
+//   return a - b
+// })
 
-const numbers = [9.81, 3.14, 100, 37]
-// Using sort method to sort number items provide a wrong result. see below
-console.log(numbers.sort()) //[100, 3.14, 37, 9.81]
-numbers.sort(function (a, b) {
-  return a - b
-})
+// console.log(numbers) // [3.14, 9.81, 37, 100]
 
-console.log(numbers) // [3.14, 9.81, 37, 100]
-
-numbers.sort(function (a, b) {
-  return b - a
-})
-console.log(numbers) //[100, 37, 9.81, 3.14]
+// numbers.sort(function (a, b) {
+//   return b - a
+// })
+// console.log(numbers) //[100, 37, 9.81, 3.14]
