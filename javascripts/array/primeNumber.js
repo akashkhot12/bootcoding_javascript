2eb343b299cd821676fc8ba2a0bd172821f498b9
akashkhot12 <akashkhot03@gmail.com>
2023-06-28 13:14:13 +0530

    prime number

---
+++ javascripts/array/primeNumber.js
@@ -0,0 +1,10 @@
+let prime=7;
+
+for(let i=2; i<=prime;i++){
+    if(prime%i==0){
+        console.log("it is not prime number");
+    }
+    else{
+        console.log("it is prime number");
+    }
+}
