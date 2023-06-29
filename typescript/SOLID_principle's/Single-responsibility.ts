65c3d8fb3fd96f4f6217a1a7d32832a4c97a3baf
akashkhot12 <akashkhot03@gmail.com>
2023-06-29 11:43:38 +0530

    single level principle

--- typescript/SOLID_principle's/Single-responsibility.ts
+++ typescript/SOLID_principle's/Single-responsibility.ts
@@ -5,18 +5,15 @@ let studentName="akash",ParentsName="ashish",TeacherName="raj"
 function StudentStatus(){
    console.log(studentName);
    
-    
 }
 
  function ParentsStatus(){
     console.log(ParentsName);
-    
-
 }
 
  function TeacherStatus(){
     console.log(TeacherName);
-    
+ 
 }
 StudentStatus();
 ParentsStatus();
