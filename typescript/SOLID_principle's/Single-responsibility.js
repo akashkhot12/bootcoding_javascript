fec9f5950a0b9406ff35748ccf03b617034c97e8
akashkhot12 <akashkhot03@gmail.com>
2023-06-29 11:42:45 +0530

    single responsibilty principles

---
+++ typescript/SOLID_principle's/Single-responsibility.js
@@ -0,0 +1,13 @@
+var studentName = "akash", ParentsName = "ashish", TeacherName = "raj";
+function StudentStatus() {
+    console.log(studentName);
+}
+function ParentsStatus() {
+    console.log(ParentsName);
+}
+function TeacherStatus() {
+    console.log(TeacherName);
+}
+StudentStatus();
+ParentsStatus();
+TeacherStatus();
---
+++ typescript/SOLID_principle's/Single-responsibility.ts
@@ -0,0 +1,24 @@
+let studentName="akash",ParentsName="ashish",TeacherName="raj"
+
+
+
+function StudentStatus(){
+   console.log(studentName);
+   
+    
+}
+
+ function ParentsStatus(){
+    console.log(ParentsName);
+    
+
+}
+
+ function TeacherStatus(){
+    console.log(TeacherName);
+    
+}
+StudentStatus();
+ParentsStatus();
+TeacherStatus();
+
