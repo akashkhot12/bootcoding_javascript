da2a919c89d7dc695fa1a4c080637a0c9a09da38
akashkhot12 <akashkhot03@gmail.com>
2023-07-16 15:00:50 +0530

    classes calling

--- typescript/functions/normalFunction.js
+++ typescript/functions/normalFunction.js
@@ -1,4 +1,15 @@
-function calling(a, b) {
-    return b ? a * b : a;
-}
-console.log(calling(30, 40));
+// function calling(a:number,b?:number):number{
+//     return b?a*b:a
+// }
+// console.log(calling(30,40))
+var App = /** @class */ (function () {
+    function App(name) {
+        this.name = name;
+    }
+    App.prototype.getName = function () {
+        console.log(this.name);
+    };
+    return App;
+}());
+var a1 = new App("akash");
+a1.getName();
--- typescript/functions/normalFunction.ts
+++ typescript/functions/normalFunction.ts
@@ -1,5 +1,25 @@
-function calling(a:number,b?:number):number{
-    return b?a*b:a
+// function calling(a:number,b?:number):number{
+//     return b?a*b:a
+// }
+
+// console.log(calling(30,40))
+
+
+
+class App{
+    name
+
+    constructor(name){
+        this.name=name
+    }
+    getName(){
+        console.log(this.name)
+    }
 }
-console.log(calling(30,40))
+
+let a1 = new App("akash");
+a1.getName();
+
+
+
 
