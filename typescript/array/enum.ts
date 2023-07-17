ff09ac570bbe18acc39c5f11d60f042246214869
akashkhot12 <akashkhot03@gmail.com>
2023-07-17 14:30:31 +0530

    enum method

---
+++ typescript/array/enum.ts
@@ -0,0 +1,20 @@
+enum media {
+    Newspaper,
+    Newsletter,
+    Magazine,
+    Book
+  }
+
+
+  console.log(media);
+  
+
+
+  enum printer {
+    Newspaper = 1,
+    Newsletter,
+    Magazine,
+    Book
+  }
+
+  console.log(printer);
