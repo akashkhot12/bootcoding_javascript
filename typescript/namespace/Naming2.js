cb37bcac5607ee6b1402b92f5b3f0f679c661f8e
akashkhot12 <akashkhot03@gmail.com>
2023-07-18 13:02:06 +0530

    namespace second pograms

---
+++ typescript/namespace/Naming2.js
@@ -0,0 +1,16 @@
+var system;
+(function (system) {
+    var computerCycle = /** @class */ (function () {
+        function computerCycle() {
+        }
+        computerCycle.prototype.computerParts = function () {
+            var arr;
+            arr = [1, "printer", true];
+            return arr;
+        };
+        return computerCycle;
+    }());
+    system.computerCycle = computerCycle;
+})(system || (system = {}));
+var s1 = new system.computerCycle();
+console.log(s1.computerParts());
