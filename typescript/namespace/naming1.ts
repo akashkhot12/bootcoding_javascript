c1246ed2cf48775cbc3fa7e557a34489674c8352
akashkhot12 <akashkhot03@gmail.com>
2023-07-18 12:48:47 +0530

    using namespace in file

---
+++ typescript/namespace/naming1.ts
@@ -0,0 +1,11 @@
+namespace print{
+    export class lable{
+        lableprint(){
+            return " print the label ..!"
+        }
+    }
+}
+
+
+let l1=new print.lable();
+console.log(l1.lableprint());
