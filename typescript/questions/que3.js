3ce8fb0e07c8be3d3a1ee0381934fec9507f554c
akashkhot12 <akashkhot03@gmail.com>
2023-07-24 11:12:08 +0530

    print odd elements from array given number using user input.

---
+++ typescript/questions/que3.js
@@ -0,0 +1,10 @@
+// print odd elements from array given number using user input.
+function printOdd(arr) {
+    for (var index = 0; index < arr.length; index++) {
+        if (arr[index] === 81) {
+            console.log(arr[index] + " " + "this odd number!");
+        }
+    }
+}
+var a2 = printOdd([10, 21, 31, 4, 5, 6, 7, 81, 9, 10]);
+a2;
---
+++ typescript/questions/que3.ts
@@ -0,0 +1,12 @@
+// print odd elements from array given number using user input.
+
+function printOdd(arr:any){
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index]===81) {
+            console.log(arr[index]+" "+"this odd number!");
+        }
+        
+    }
+}
+let a2=printOdd([10,21,31,4,5,6,7,81,9,10])
+a2;
