f2c2f48665f962dcf7b1930ded5f3b85f8481916
akashkhot12 <akashkhot03@gmail.com>
2023-07-25 07:57:28 +0530

    condition

---
+++ javascripts/self_learning/checkCondition.js
@@ -0,0 +1,31 @@
+// let number=0;
+let msg
+
+// if(number>0){
+//     msg="number is positive"
+// }
+// else if (number==0) {
+//     msg = "number is equal to zero"
+// }
+// else{
+//     msg = "number is negative"
+// }
+
+// console.log(msg);
+
+function checkConditon(number) {
+    if(number>0){
+    msg="number is positive"
+}
+else if (number==0) {
+    msg = "number is equal to zero"
+}
+else{
+    msg = "number is negative"
+}
+
+console.log(msg);
+    
+}
+
+let w1=checkConditon(-55)
