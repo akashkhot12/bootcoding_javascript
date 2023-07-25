a61d229d5549925b2ec63c233ba8ae6d2a7399b7
akashkhot12 <akashkhot03@gmail.com>
2023-07-25 08:18:25 +0530

    first letter capital

--- javascripts/self_learning/firstNameSplice.js
+++ javascripts/self_learning/firstNameSplice.js
@@ -1,7 +1,19 @@
-let name="ramNAtH"
-let firstLetter = name.slice(0,1);
+// let name="ramNAtH"
+// let firstLetter = name.slice(0,1);
+// let FirstLetterCapital = firstLetter.toUpperCase()
+// let remainingLetter = name.slice(1,name.length)
+// let remainingWords = remainingLetter.toLowerCase()
+// let FullLetter = FirstLetterCapital + remainingWords
+// console.log(FullLetter);
+
+
+function firstLetterAlphabet(name){
+    let firstLetter = name.slice(0,1);
 let FirstLetterCapital = firstLetter.toUpperCase()
 let remainingLetter = name.slice(1,name.length)
 let remainingWords = remainingLetter.toLowerCase()
 let FullLetter = FirstLetterCapital + remainingWords
 console.log(FullLetter);
+}
+
+let a1=firstLetterAlphabet("ryson")
