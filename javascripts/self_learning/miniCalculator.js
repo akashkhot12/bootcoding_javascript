03391194cd08aa973763ed1de1cf6dbaae17cb86
akashkhot12 <akashkhot03@gmail.com>
2023-07-25 07:58:02 +0530

    mini calculator

---
+++ javascripts/self_learning/miniCalculator.js
@@ -0,0 +1,19 @@
+let operator = "%"
+let number1= 2;
+let number2= 2;
+let num
+
+if(operator==="+"){
+    num=number1+number2;
+}
+else if (operator==="-") {
+    num=number1-number2;
+}
+else if (operator==="*") {
+    num=number1*number2;
+}
+else if (operator==="%") {
+    num=number1%number2;
+}
+
+console.log(num)
