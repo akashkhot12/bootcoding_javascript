5ba0d3f16701873f8946ef7f64ce402723644ea5
akashkhot12 <akashkhot03@gmail.com>
2023-07-26 12:26:34 +0530

    leap year or not

---
+++ javascripts/practice/LeapYear.js
@@ -0,0 +1,30 @@
+function LeapYear(year){
+    if (year%4==0) {
+       if(year%100==0){
+        if(year%400==0){
+            statement()
+        }
+        else {
+            cFalseStatement();
+        }
+       }
+       else
+       {
+        statement()
+       }
+    }
+    else{
+        FalseStatement();
+    }
+}
+let leapOrNot = LeapYear (1245);
+
+function statement(){
+    console.log(
+        "this is leap year"
+    );
+}
+
+function FalseStatement(){
+    console.log("Not leap year");
+}
