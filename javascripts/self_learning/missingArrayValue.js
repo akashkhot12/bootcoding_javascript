3fa73406c9c5336f1d8ec549441e8937b6c5082d
akashkhot12 <akashkhot03@gmail.com>
2023-07-26 11:22:40 +0530

    lopping statements

--- javascripts/self_learning/missingArrayValue.js
+++ javascripts/self_learning/missingArrayValue.js
@@ -1,6 +1,8 @@
 let num=5;
 let arr=[1,2,3,5]
 
+
 for (let index = 0; index < arr.length; index++) {
-    
+    for(let i= 0 ;i<num ;i++)
+    return i ;
 }
---
+++ javascripts/self_learning/printForLoop.js
@@ -0,0 +1,5 @@
+let car = ["audi","bmw" ,"verna"]
+
+for(let i=0;i<car.length;i++){
+    console.log(car[i]);
+}
