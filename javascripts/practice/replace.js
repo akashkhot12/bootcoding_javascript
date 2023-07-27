eeacf2d8e39863dd1b3a5fd29c678625ebb19576
akashkhot12 <akashkhot03@gmail.com>
2023-07-27 09:59:17 +0530

    reaplce the second element

---
+++ javascripts/practice/replace.js
@@ -0,0 +1,12 @@
+// var str = "xyz"
+// var char = str.split("")
+// char.splice(1, 1, "a");
+// console.log(char);
+
+
+function replace(string){
+   var str = string.split("");
+    str.splice(1,1,"a");
+    console.log(str);
+}
+let ak = replace("xyz")
