780e053763c9b5171ffc31d70035cb44a71d5db7
akashkhot12 <akashkhot03@gmail.com>
2023-07-27 10:17:04 +0530

    resubmition

--- javascripts/practice/replace.js
+++ javascripts/practice/replace.js
@@ -1,12 +1,7 @@
-// var str = "xyz"
-// var char = str.split("")
-// char.splice(1, 1, "a");
-// console.log(char);
-
 
 function replace(string){
    var str = string.split("");
-    str.splice(1,1,"a");
-    console.log(str);
+ str.splice(1,1,"a")
+    console.log(str.join(""));
 }
 let ak = replace("xyz")
