a26b8212a1bb4f2e5b951fdebb00ed555c01db7f
akashkhot12 <akashkhot03@gmail.com>
2023-07-28 08:21:27 +0530

    checklast digit

---
+++ javascripts/practice/checkLastDigit.js
@@ -0,0 +1,14 @@
+let a = 5 ;
+let b=  55 ; 
+let c = 654;
+
+let res1 = a%10;
+let res2 = b%10;
+let res3 = c%10;
+
+if (res1==res2 && res1==res3) {
+    console.log("the last digit is same");
+}
+else {
+    console.log("this not mctch");
+}
