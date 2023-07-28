ccf2ed771cc55ef0b4539d41b3e342fbca3a854a
akashkhot12 <akashkhot03@gmail.com>
2023-07-28 09:01:59 +0530

    for eaqch loop

---
+++ javascripts/practice/forEach.js
@@ -0,0 +1,5 @@
+let array = ["akash","rahul" ,"vinode","yash"]
+
+array.forEach(function(value,i){
+    console.log(i +" = "+value);
+})
---
+++ javascripts/practice/sumOfElement.js
@@ -0,0 +1,9 @@
+
+array = [123,345,126];
+for(let i =0 ; i<array.length;i++){
+    let first = array[0];
+    first += first;
+    console.log(first);
+}
+
+
