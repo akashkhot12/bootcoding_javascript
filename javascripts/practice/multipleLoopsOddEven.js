0c76614ac0eb08d7c848b93de3b5df6a804763ba
akashkhot12 <akashkhot03@gmail.com>
2023-07-28 14:11:35 +0530

    even odd

---
+++ javascripts/practice/multipleLoopsOddEven.js
@@ -0,0 +1,12 @@
+// odd methodes
+let array = [4,3,5]
+
+array.forEach(element  =>{
+    if(element%2==0) {
+        console.log("this number is even number");
+    }
+    else{
+        console.log("this is Odd number");
+    }
+}  
+);
