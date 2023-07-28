438c327fd2f1f0a56069ca187663d31a496493e5
akashkhot12 <akashkhot03@gmail.com>
2023-07-28 14:22:10 +0530

    multiple loops using the even odd number

--- javascripts/practice/multipleLoopsOddEven.js
+++ javascripts/practice/multipleLoopsOddEven.js
@@ -1,7 +1,18 @@
 // odd methodes
 let array = [4,3,5]
 
-array.forEach(element  =>{
+// array.forEach(element  =>{
+//     if(element%2==0) {
+//         console.log("this number is even number");
+//     }
+//     else{
+//         console.log("this is Odd number");
+//     }
+// }  
+// );
+
+
+for (const element of array) {
     if(element%2==0) {
         console.log("this number is even number");
     }
@@ -9,4 +20,4 @@ array.forEach(element  =>{
         console.log("this is Odd number");
     }
 }  
-);
+
