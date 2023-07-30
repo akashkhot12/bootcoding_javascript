994cba8bd5d4e7bf370ebdac7acbf534b3386dc2
akashkhot12 <akashkhot03@gmail.com>
2023-07-30 13:16:41 +0530

    open api works

--- javascripts/FetchApi using js/api.js
+++ javascripts/FetchApi using js/api.js
@@ -1,7 +1,7 @@
-let api = fetch("https://goweather.herokuapp.com//weather/Ny")
+let api = fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
 
 api.then((value1)=>{
     return value1.json()
-}),then((value2)=>{
+}).then((value2)=>{
     console.log(value2);
 })
