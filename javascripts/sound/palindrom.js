4368a33d9b3f3d4e74c04fda556a6b6a46acc770
akashkhot12 <akashkhot03@gmail.com>
2023-08-01 11:30:40 +0530

    palindrome number

--- javascripts/sound/palindrom.js
+++ javascripts/sound/palindrom.js
@@ -1,12 +1,4 @@
-function findRoot(number){
-    for (let index = 0; index*index <= number; index++) {
-       if (index*index ===number) {
-        return index;
-       }
-    }
-}
 
-console.log(findRoot(16));
 
 // palindrome number ;
 
@@ -14,18 +6,18 @@ console.log(findRoot(16));
 
 // }
 
-// const palindrome = (n) => {
+const palindrome = (n) => {
 
-//   let sum = 0,rem , temp = n ; 
-//   while (n != 0) {
-//     rem = n % 10;
-//     sum = (sum * 10) + rem;
-//     n = Math.floor(n / 10);
-//   }
-//   if (temp == sum) {
-//     return "its palindrom";
-//   } else {
-//     return "its not a palindrom";
-//   }
-// };
-// console.log(palindrome(121));
+  let sum = 0,rem , temp = n ; 
+  while (n != 0) {
+    rem = n % 10;
+    sum = (sum * 10) + rem;
+    n = Math.floor(n / 10);
+  }
+  if (temp == sum) {
+    return "its palindrom";
+  } else {
+    return "its not a palindrom";
+  }
+};
+console.log(palindrome(121));
