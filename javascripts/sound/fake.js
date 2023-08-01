c828793d6791ce2e1fc8b968618e7ac9743dc74e
akashkhot12 <akashkhot03@gmail.com>
2023-08-01 13:04:10 +0530

    find even or odd number

--- javascripts/sound/fake.js
+++ javascripts/sound/fake.js
@@ -4,3 +4,18 @@
 //     i++;
 //     console.log(i);
 // },1000)
+
+
+// even or odd
+let num = 1233 ;
+if (num%2==0) {
+    console.log("this number is even number");
+}
+else if(num%2!=0){
+    console.log("this is odd number");
+}
+else{
+    console.log("this value is not integer");
+}
+
+
