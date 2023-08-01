734d8a63417c155156e07146a43ebe24a09c41b7
akashkhot12 <akashkhot03@gmail.com>
2023-08-01 14:57:49 +0530

    fnd armstrong number

--- javascripts/self_learning/sqrt.js
+++ javascripts/self_learning/sqrt.js
@@ -6,9 +6,31 @@
 
 
 
-function sqrt(number) {
-    b=Math.sqrt(number)
-  console.log(b);
+// function sqrt(number) {
+//     b=Math.sqrt(number)
+//   console.log(b);
+// }
+
+// let a1=sqrt(25)
+
+
+
+function isArmstrong(number) {
+  const digits = number.toString().split('');
+  const order = digits.length;
+  const sum = digits.reduce(
+      (acc, digit) =>
+          acc + Math.pow(parseInt(digit), order), 0);
+
+  if (sum === number) {
+      console.log(
+          number + " is an Armstrong Number");
+  }
+  else {
+      console.log
+          (number + " is not an Armstrong Number");
+  }
 }
 
-let a1=sqrt(25)
+isArmstrong(9474);
+
