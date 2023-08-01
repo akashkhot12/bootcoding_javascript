653bf4c4545f7f1078bd70405edb5ec0a7c78382
akashkhot12 <akashkhot03@gmail.com>
2023-08-01 13:31:16 +0530

    prime number

--- javascripts/sound/fake.js
+++ javascripts/sound/fake.js
@@ -7,15 +7,24 @@
 
 
 // even or odd
-let num = 1233 ;
-if (num%2==0) {
-    console.log("this number is even number");
-}
-else if(num%2!=0){
-    console.log("this is odd number");
-}
-else{
-    console.log("this value is not integer");
-}
+// let num = 1233 ;
+// if (num%2==0) {
+//     console.log("this number is even number");
+// }
+// else if(num%2!=0){
+//     console.log("this is odd number");
+// }
+// else{
+//     console.log("this value is not integer");
+// }
 
+
+// prime number ; 
+let num =7;
+
+if (num!=0) {
+    if (num%) {
+        
+    }
+}
 
