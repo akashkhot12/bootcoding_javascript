6e2cf47e7554c37659dc9ac7e246c9dd52964681
akashkhot12 <akashkhot03@gmail.com>
2023-08-02 12:30:59 +0530

    right angle star pattern

--- javascripts/Star Patterns/rightAngle.js
+++ javascripts/Star Patterns/rightAngle.js
@@ -37,3 +37,17 @@
 // console.log(star);
 
 
+const pattern=(n)=>{
+    star=""
+    for(let row =1;row<=n;row++){
+        for(let space =1;space<=n-row;space++){
+            star+=" "
+        }
+        for(let col=1;col<=row;col++){
+            star+="*"
+        }
+        star+="\n"
+    }
+    return star;
+}
+console.log(pattern(30));
