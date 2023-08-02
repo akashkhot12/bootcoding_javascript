764d40615fd161bd44fe4ed57d020988fb31be11
akashkhot12 <akashkhot03@gmail.com>
2023-08-02 09:46:15 +0530

    maximum and minimum catch object

--- javascripts/math object/object.js
+++ javascripts/math object/object.js
@@ -1,9 +1,14 @@
 // all math object try to clear doubts
 
-const Pi = Math.PI;
-console.log(Pi);
+// const Pi = Math.PI;
+// console.log(Pi);
 
-console.log(Math.round(9.99));
-console.log(Math.floor(Pi));
-console.log(Math.ceil(Pi));
+// console.log(Math.round(9.99));
+// console.log(Math.floor(Pi));
+// console.log(Math.ceil(Pi));
 
+
+// max object 
+
+console.log(Math.max(1,2,3,-45,));
+console.log(Math.min(1,2,3,-45,));
