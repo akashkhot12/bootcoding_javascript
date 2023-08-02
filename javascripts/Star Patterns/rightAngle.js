bf95b1ed2656b2699dad207b6b49043e5a93c999
akashkhot12 <akashkhot03@gmail.com>
2023-08-02 12:26:54 +0530

    another pattern

--- javascripts/Star Patterns/rightAngle.js
+++ javascripts/Star Patterns/rightAngle.js
@@ -1,24 +1,39 @@
-let string = "";
-for (let row = 1; row <=4; row++) {
-   for(let col =1 ; col<=row;col++){
-    string+="*"
-   }
-   string+="\n"
-}
-console.log(string);
+// let string = "";
+// for (let row = 1; row <=4; row++) {
+//    for(let col =1 ; col<=row;col++){
+//     string+="*"
+//    }
+//    string+="\n"
+// }
+// console.log(string);
 
 
 
-// user input
+// // user input
 
-const pattern = (n)=>{
-    let string = ""
-    for(let row = 1 ; row<=n;row++){
-        for(let col=1 ; col<=row;col++){
-            string+="*"
-        }
-        string+="\n"
-    }
-    return string;
-}
-console.log(pattern(70));
+// const pattern = (n)=>{
+//     let string = ""
+//     for(let row = 1 ; row<=n;row++){
+//         for(let col=1 ; col<=row;col++){
+//             string+="*"
+//         }
+//         string+="\n"
+//     }
+//     return string;
+// }
+// console.log(pattern(70));
+
+
+// let star = "";
+// for(let row = 1;row<=3;row++){
+//     for(let space =1;space<=3-row;space++){
+//         star+=" "
+//     }
+//     for(let col=1;col<=row;col++){
+//         star+="*"
+//     }
+//     star+="\n";
+// }
+// console.log(star);
+
+
