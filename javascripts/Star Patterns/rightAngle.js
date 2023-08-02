66ef13cd5e428fd3ec598c211ac4317af1ba0d02
akashkhot12 <akashkhot03@gmail.com>
2023-08-02 11:14:32 +0530

    user input star pattern

--- javascripts/Star Patterns/rightAngle.js
+++ javascripts/Star Patterns/rightAngle.js
@@ -6,3 +6,19 @@ for (let row = 1; row <=4; row++) {
    string+="\n"
 }
 console.log(string);
+
+
+
+// user input
+
+const pattern = (n)=>{
+    let string = ""
+    for(let row = 1 ; row<=n;row++){
+        for(let col=1 ; col<=row;col++){
+            string+="*"
+        }
+        string+="\n"
+    }
+    return string;
+}
+console.log(pattern(70));
