d6e4c925031fcbfec4dee6e4ceb51792a57ed37a
akashkhot12 <akashkhot03@gmail.com>
2023-08-02 10:47:21 +0530

    square pattern

---
+++ javascripts/Star Patterns/square_pattern.js
@@ -0,0 +1,11 @@
+const pattern = (n,p)=>{
+    let string = ""
+    for (let index = 1; index <=n;index++) {
+        for (let j = 1; j <= p; j++) {
+           string+="*"; 
+        }
+        string+="\n"
+    }
+    return string;
+}
+console.log(pattern(5,6));
