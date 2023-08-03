2f8665e493af2e5ca4a4db23b51bbbd6463dbe01
akashkhot12 <akashkhot03@gmail.com>
2023-08-03 12:52:15 +0530

    star pattern

--- javascripts/Star Patterns/starPattern3.js
+++ javascripts/Star Patterns/starPattern3.js
@@ -1,14 +1,14 @@
 let str ="";
-// let str1="";
-// for(let row = 1;row<=4;row++){
-//     for(let spc=1;spc<=4-row;spc++){
-//         str+=" ";
-//     }
-//     for(let col=1;col<=row;col++){
-//         str+="*"
-//     }
-//     str+="\n"
-// }
+let str1="";
+for(let row = 1;row<=4;row++){
+    for(let spc=1;spc<=4-row;spc++){
+        str+=" ";
+    }
+    for(let col=1;col<=row;col++){
+        str+="*"
+    }
+    str+="\n"
+}
 for(let row1=1;row1<=4;row1++){
     for(let spc1=1;spc1<=row1;spc1++){
         str+=" ";
@@ -21,16 +21,5 @@ for(let row1=1;row1<=4;row1++){
 console.log(str);
 
 
-// str="";
-// for(let row=1;row<=4;row++){
-//     for(let sp=1;sp>=4-row;sp--){
-//         str+="-"
-//     }
-//     // for(let col=2;col<=row;row++){
-//     //     str+="*"
-//     // }
-//     str+="\n"
-// }
-// console.log(str);
 
 
