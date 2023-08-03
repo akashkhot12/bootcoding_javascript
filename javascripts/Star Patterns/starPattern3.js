145c9d9ede4fc53c80e137efef864c69f3e9b2cd
akashkhot12 <akashkhot03@gmail.com>
2023-08-03 13:01:52 +0530

    start pattern using number

--- javascripts/Star Patterns/starPattern3.js
+++ javascripts/Star Patterns/starPattern3.js
@@ -1,25 +1,46 @@
-let str ="";
-let str1="";
-for(let row = 1;row<=4;row++){
-    for(let spc=1;spc<=4-row;spc++){
-        str+=" ";
+// let str ="";
+// for(let row = 1;row<=4;row++){
+//     for(let spc=1;spc<=4-row;spc++){
+//         str+=" ";
+//     }
+//     for(let col=1;col<=row;col++){
+//         str+="*"
+//     }
+//     str+="\n"
+// }
+// for(let row1=1;row1<=4;row1++){
+//     for(let spc1=1;spc1<=row1;spc1++){
+//         str+=" ";
+//     }
+//     for(let col=1;col<=4-row1;col++){
+//         str+="*"
+//     }
+//     str+="\n"
+// }
+// console.log(str);
+
+
+let str= ""
+const GetTriangle=(num)=>{
+    for(let row = 1;row<=num;row++){
+        for(let spc=1;spc<=num-row;spc++){
+            str+=" ";
+        }
+        for(let col=1;col<=row;col++){
+            str+="*"
+        }
+        str+="\n"
     }
-    for(let col=1;col<=row;col++){
-        str+="*"
+    for(let row1=1;row1<=num;row1++){
+        for(let spc1=1;spc1<=row1;spc1++){
+            str+=" ";
+        }
+        for(let col=1;col<=num-row1;col++){
+            str+="*"
+        }
+        str+="\n"
     }
-    str+="\n"
+    return str;
 }
-for(let row1=1;row1<=4;row1++){
-    for(let spc1=1;spc1<=row1;spc1++){
-        str+=" ";
-    }
-    for(let col=1;col<=4-row1;col++){
-        str+="*"
-    }
-    str+="\n"
-}
-console.log(str);
-
-
-
+console.log(GetTriangle(4));
 
