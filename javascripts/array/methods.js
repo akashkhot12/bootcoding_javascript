17d8211edbaa52ab0527128a8be68bf3c9479792
akashkhot12 <akashkhot03@gmail.com>
2023-08-04 12:47:50 +0530

    remove duplicated sorted array

--- javascripts/array/methods.js
+++ javascripts/array/methods.js
@@ -1,3 +1,10 @@
-let js = 'JavaScript'
-const charsInJavaScript = js.split('')
-console.log(charsInJavaScript);
+const removeDuplicates = function (nums) {
+   let newUnique = [ ...new Set(nums)];
+   return newUnique;
+   };
+   console.log(removeDuplicates([1,1,2]));
+
+
+// let array = [1,1,2,3,4,5,4,4,4,6,7,7]
+// let uni = [...new Set(array)]
+// console.log(uni);
