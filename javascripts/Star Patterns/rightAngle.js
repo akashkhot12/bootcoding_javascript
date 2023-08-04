fc0c40d9ba59b3990fa9388166bcb2682b02f0ed
akashkhot12 <akashkhot03@gmail.com>
2023-08-04 10:35:52 +0530

    right angle triangle

--- javascripts/Star Patterns/rightAngle.js
+++ javascripts/Star Patterns/rightAngle.js
@@ -12,21 +12,18 @@
 // console.log(star);
 
 
-// const pattern=(n)=>{
-//     star=""
-//     for(let row =1;row<=n;row++){
-//         for(let space =1;space<=n-row;space++){
-//             star+=" "
-//         }
-//         for(let col=1;col<=row;col++){
-//             star+="*"
-//         }
-//         star+="\n"
-//     }
-//     return star;
-// }
-// console.log(pattern(30));
+const pattern=(n)=>{
+    star=""
+    for(let row =1;row<=n;row++){
+        for(let space =1;space<=n-row;space++){
+            star+=" "
+        }
+        for(let col=1;col<=row;col++){
+            star+="*"
+        }
+        star+="\n"
+    }
+    return star;
+}
+console.log(pattern(30));
 
-const numbers = [0, 3.14, 9.81, 37, 98.6, 100];
-console.log('Numbers:', numbers);
-console.log('Number of numbers:', numbers.length)
