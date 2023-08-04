b56326e9af026d0cc3d0e97e61b450022fb59e33
akashkhot12 <akashkhot03@gmail.com>
2023-08-04 13:24:59 +0530

    dom

--- javascripts/array/methods.js
+++ javascripts/array/methods.js
@@ -1,10 +1,23 @@
-const removeDuplicates = function (nums) {
-   let newUnique = [ ...new Set(nums)];
-   return newUnique;
-   };
-   console.log(removeDuplicates([1,1,2]));
+// const removeDuplicates = function (nums) {
+//    let newUnique = [ ...new Set(nums)];
+//    return newUnique;
+//    };
+//    console.log(removeDuplicates([1,1,2]));
 
 
 // let array = [1,1,2,3,4,5,4,4,4,6,7,7]
 // let uni = [...new Set(array)]
 // console.log(uni);
+
+// Remove Element
+
+let array = [1,2,3,1,2,3]
+console.log(array);
+console.log(array.sort());
+let num = 3
+for (let index = 0; index < array.length; index++) {
+    if (array[index]===num) {
+        array.splice(num)
+    }
+    return array;
+}
---
+++ javascripts/html/index.js
@@ -0,0 +1,5 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let para = document.getElementsByClassName("paragraph");
+
+
