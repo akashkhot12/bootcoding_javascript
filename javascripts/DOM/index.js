bf624a6a9e2f739d52bfba68a484b3261631cc7f
akashkhot12 <akashkhot03@gmail.com>
2023-08-05 13:38:07 +0530

    event listeners

--- javascripts/DOM/index.js
+++ javascripts/DOM/index.js
@@ -1,32 +1,18 @@
+const p =document.querySelector('#para')
+const s =document.querySelector('#span')
+const b =document.querySelector('#btn')
 
-const p = document.querySelector("#para");
-const s = document.querySelector("#span");
-const b = document.querySelector("#btn");
-
-// if (b.innerHTML === "Less") {
-//   b.addEventListener("click", () => {
-//     s.style.display = "none";
-//     b.innerHTML = "More";
-//   });
-// }else if(b.innerHTML === "More"){
-//   b.addEventListener("click", () => {
-//     s.style.display = "block";
-//     b.innerHTML = "Less";
-//   });
-// }
-
-
-const content = () =>{
-    b.addEventListener("click", () => {
-           if(b.innerHTML === "Less"){
-            s.style.display = "none";
-            b.innerHTML = "More";
-           }
-           else{
-            s.style.display = "inline";
-            b.innerHTML = "Less";
-           }
-          })
+let getData=()=>{
+    b.addEventListener('click',()=>{
+        if (b.innerHTML==="Less") {
+            s.style.display="none"
+            b.innerHTML = "More"
+        }
+        else{
+            s.style.display="inline"
+            b.innerHTML ="Less"
+        }
+    })
 }
 
-content()
+getData()
