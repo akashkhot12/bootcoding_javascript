1002ccb2a93f4aee9a49d7a7a31e081ea8d4f1a7
akashkhot12 <akashkhot03@gmail.com>
2023-08-05 13:26:32 +0530

    change event

--- javascripts/DOM/index.js
+++ javascripts/DOM/index.js
@@ -1,8 +1,32 @@
-let h1 = document.getElementById('heading');
-let btn = document.querySelector('.btn')
 
-btn.addEventListener('click',()=>{
-    h1.innerHTML = "Chal bhaag";
-})
+const p = document.querySelector("#para");
+const s = document.querySelector("#span");
+const b = document.querySelector("#btn");
 
-    
+// if (b.innerHTML === "Less") {
+//   b.addEventListener("click", () => {
+//     s.style.display = "none";
+//     b.innerHTML = "More";
+//   });
+// }else if(b.innerHTML === "More"){
+//   b.addEventListener("click", () => {
+//     s.style.display = "block";
+//     b.innerHTML = "Less";
+//   });
+// }
+
+
+const content = () =>{
+    b.addEventListener("click", () => {
+           if(b.innerHTML === "Less"){
+            s.style.display = "none";
+            b.innerHTML = "More";
+           }
+           else{
+            s.style.display = "inline";
+            b.innerHTML = "Less";
+           }
+          })
+}
+
+content()
