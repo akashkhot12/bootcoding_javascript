da693980b4bd660e8c3f092910efa764a9220125
akashkhot12 <akashkhot03@gmail.com>
2023-08-05 13:51:31 +0530

    changing colors

--- javascripts/DOM/index.js
+++ javascripts/DOM/index.js
@@ -2,17 +2,32 @@ const p =document.querySelector('#para')
 const s =document.querySelector('#span')
 const b =document.querySelector('#btn')
 
-let getData=()=>{
-    b.addEventListener('click',()=>{
-        if (b.innerHTML==="Less") {
-            s.style.display="none"
-            b.innerHTML = "More"
-        }
-        else{
-            s.style.display="inline"
-            b.innerHTML ="Less"
-        }
-    })
-}
+// let getData=()=>{
+//     b.addEventListener('click',()=>{
+//         if (b.innerHTML==="Less") {
+//             s.style.display="none"
+//             b.innerHTML = "More"
+//         }
+//         else{
+//             s.style.display="inline"
+//             b.innerHTML ="Less"
+//         }
+//     })
+// }
 
-getData()
+// getData()
+
+
+
+
+// event.js
+
+let para  = document.querySelector('#change');
+
+para.addEventListener('click',()=>{
+    para.style.color = "red";
+    para.style.fontSize = "60px";
+})
+
+
+console.log("events");
