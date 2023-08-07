9df9971c94f79fc0b8e9c38517475f1428e6cdce
akashkhot12 <akashkhot03@gmail.com>
2023-08-07 11:51:49 +0530

    solve the adding two numbers

--- javascripts/leetcode/2AddTwoNumbers.js
+++ javascripts/leetcode/2AddTwoNumbers.js
@@ -1,11 +1,4 @@
-// var addTwoNumbers = function(l1, l2) {
-    
-// };
-
-
-let l1 = [2,4,3]
-let l2 = [5,6,4]
-
+var addTwoNumbers = function(l1, l2) {
 l1 = l1.reverse();
 l1 = l1.join('');
 l1 = parseInt(l1)
@@ -17,7 +10,26 @@ let res = l1+l2;
 // console.log(res); 807;
 let arrayOfDigits = Array.from(String(res), Number);
 arrayOfDigits = arrayOfDigits.reverse();
-console.log(arrayOfDigits);
+return arrayOfDigits;
+}
+console.log(addTwoNumbers([2,4,3],[5,6,4]));
+
+
+// let l1 = [2,4,3]
+// let l2 = [5,6,4]
+
+// l1 = l1.reverse();
+// l1 = l1.join('');
+// l1 = parseInt(l1)
+// l2 = l2.reverse();
+// l2 = l2.join('');
+// l2 = parseInt(l2)
+
+// let res = l1+l2;
+// // console.log(res); 807;
+// let arrayOfDigits = Array.from(String(res), Number);
+// arrayOfDigits = arrayOfDigits.reverse();
+// console.log(arrayOfDigits);
 
 
 
