a2475337554a37ba1ef129bc0e4d05320cdeeadc
akashkhot12 <akashkhot03@gmail.com>
2023-08-09 10:26:50 +0530

    looping statements

---
+++ javascripts/JavaScript/loopingStatements.js
@@ -0,0 +1,33 @@
+// while loop
+// let a =10;
+// while (a>=1) {
+//     console.log(a*2);
+//     a = a-1
+// }
+
+
+// do while loop
+// let b = 1;
+// do {
+//     console.log(b);
+//     b++;
+// } while (b<=10);
+
+
+
+// switch case
+// let age = 30;
+// switch (true) {
+//     case (age==30): console.log("u r good");
+//         break;
+//     default: console.log("not good");
+//         break;
+// }
+
+
+
+// forOf
+
+for (const a of b<=10) {
+    console.log(a);
+}
