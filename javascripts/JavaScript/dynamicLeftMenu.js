8d545999a83ce59106dd66a3f22ac7ff7ef4bf7b
akashkhot12 <akashkhot03@gmail.com>
2023-08-11 11:00:05 +0530

    call back funtion

--- javascripts/JavaScript/dynamicLeftMenu.js
+++ javascripts/JavaScript/dynamicLeftMenu.js
@@ -1,11 +1,28 @@
 let items = [];
-function getName(){
-    Namevalue = document.querySelector("#Name").value;
-    items.push(Namevalue);  
-    display();
-}
-function display() {
-    for (const value of items) {
-        document.getElementById("demo").innerHTML = `<li>${value}</li>`
-    }
-}
+
+// function display(value) {
+//     for (const value of items) {
+//         document.getElementById("demo").innerHTML = `<li>${value}</li>`
+//     }
+// }
+// function getName(){
+//     Namevalue = document.querySelector("#Name").value;
+//     items.push(Namevalue);  
+//     return items;
+// }
+
+// let result = getName();
+// display(result)
+
+function myDisplayer(value) {
+    document.getElementById("demo").innerHTML = value;
+  }
+  
+  function getName() {
+   let Namevalue = document.querySelector("#Name").value;
+        items.push(Namevalue);  
+        return items;
+  }
+  
+  let result = getName();
+  myDisplayer(result);
