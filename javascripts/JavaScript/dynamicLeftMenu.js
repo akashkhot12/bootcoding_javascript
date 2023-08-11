c8da776e9050f1a9521f4e7d744b4c77978742fe
akashkhot12 <akashkhot03@gmail.com>
2023-08-11 12:24:32 +0530

    dynamic menu

--- javascripts/JavaScript/dynamicLeftMenu.js
+++ javascripts/JavaScript/dynamicLeftMenu.js
@@ -1,15 +1,19 @@
 let items = [];
+let Namevalue = document.querySelector("#Name")
+const ul = document.getElementById('menu')
 function getName(){
-    Namevalue = document.querySelector("#Name").value;
-    items.push(Namevalue);  
+    let val = Namevalue.value;
+    items.push(val);  
+    
     display();
 }
 function display() {
-    
-    for (let i = 0; i < items.length; i++) {
-        let element = document.getElementById("demo")
-        element.innerHTML = items[i];
+    ul.setAttribute('id','theList')
+    for (let i = 0; i <=items.length-1; i++) {
+        const li = document.getElementById('demo')
+        li.innerHTML = items[i];
+        ul.appendChild(li)
       }
-      return element;
+      cont.appendChild(ul)
 }
 
