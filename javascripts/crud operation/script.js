08a6aee9021e8a2849211578bba494c5fc7aeee3
akashkhot12 <akashkhot03@gmail.com>
2023-08-15 11:48:37 +0530

    create dynamic list

--- javascripts/crud operation/script.js
+++ javascripts/crud operation/script.js
@@ -14,3 +14,26 @@ function readFormData(){
     formData["perPrice"] = document.getElementById("perPrice").ariaValueMax;
     return formData;  
 }
+
+
+// insert the data 
+
+function insertNewRecord(data){
+ var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+
+ var newRow = table.insertRow(table.length);
+ var cell1 =  newRow.insertCell(0);
+    cell1.innerHTML=data.productCode;
+
+    var cell2 =  newRow.insertCell(1);
+    cell2.innerHTML=data.product;
+
+    var cell3 =  newRow.insertCell(2);
+    cell3.innerHTML=data.qty;
+
+    var cell4 =  newRow.insertCell(3);
+    cell4.innerHTML=data.perPrice;
+
+    var cell5 =  newRow.insertCell(3);
+    cell5.innerHTML=`<buton>Edit</buton> <button>Delete</button>`;
+}
