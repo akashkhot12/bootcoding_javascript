535063e07a04e5b0a89d015eaf891baca7389327
akashkhot12 <akashkhot03@gmail.com>
2023-08-15 11:40:55 +0530

    retrivee the data

--- javascripts/crud operation/script.js
+++ javascripts/crud operation/script.js
@@ -1,3 +1,16 @@
+var selectedRow = null;
+
 function onFormSubmit(){
-    
+
+}
+
+// retrive the data 
+
+function readFormData(){
+    var formData = {}
+    formData["productCode"] = document.getElementById("productCode").ariaValueMax;
+    formData["product"] = document.getElementById("product").ariaValueMax;
+    formData["qty"] = document.getElementById("qty").ariaValueMax;
+    formData["perPrice"] = document.getElementById("perPrice").ariaValueMax;
+    return formData;  
 }
