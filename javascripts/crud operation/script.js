6831ed4265f1a4db0a06fe75ddbbd7451a12f5b0
akashkhot12 <akashkhot03@gmail.com>
2023-08-15 12:01:13 +0530

    edit and delete method

--- javascripts/crud operation/script.js
+++ javascripts/crud operation/script.js
@@ -1,7 +1,14 @@
 var selectedRow = null;
 
-function onFormSubmit(){
+function onFormSubmit(e){
+     event.preventDefault();
+     var formData = readFormData();
+     if(selectedRow===null){
+        insertNewRecord(formData);
+     }
+     else{
 
+     }
 }
 
 // retrive the data 
@@ -35,5 +42,27 @@ function insertNewRecord(data){
     cell4.innerHTML=data.perPrice;
 
     var cell5 =  newRow.insertCell(3);
-    cell5.innerHTML=`<buton>Edit</buton> <button>Delete</button>`;
+    cell5.innerHTML=`<buton onClick="onEdit(this)">Edit</buton> <button>Delete</button>`;
+}
+
+// edit the data
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById('productCode').value = selectedRow.cells[0].innerHTML;
+
+    document.getElementById('product').value = selectedRow.cells[1].innerHTML;
+
+    document.getElementById('qty').value = selectedRow.cells[2].innerHTML;
+
+    document.getElementById('perPrice').value = selectedRow.cells[3].innerHTML;
+
+}
+
+// update
+function updateRecord(formData){
+    selectedRow.cell[0].innerHTML = formData.productCode;
+    selectedRow.cell[1].innerHTML = formData.product;
+    selectedRow.cell[2].innerHTML = formData.qty;
+    selectedRow.cell[3].innerHTML = formData.perPrice;
 }
