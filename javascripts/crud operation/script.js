43d9538b86f7ecf6bdc4017b175ba92c66e85607
akashkhot12 <akashkhot03@gmail.com>
2023-08-16 14:13:35 +0530

    scripting

---
+++ javascripts/crud operation/script.js
@@ -0,0 +1,6 @@
+function validForm() {
+    var name = document.getElementById("name").value
+    var age = document.getElementById("age").value
+    var address = document.getElementById("address").value
+    var email = document.getElementById("email").value
+}
