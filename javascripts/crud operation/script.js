525ae5fdc4ebe58a6df7be0895c1e713cd17b824
akashkhot12 <akashkhot03@gmail.com>
2023-08-16 14:48:29 +0530

    scripting

--- javascripts/crud operation/script.js
+++ javascripts/crud operation/script.js
@@ -32,3 +32,62 @@ function validForm() {
 
     return true;
 }
+
+function showData(){
+    var peopleList;
+    if(localStorage.getItem("peopleList")==null){
+        peopleList = [];
+
+    }
+    else{
+        peopleList =JSON.parse(localStorage.getItem("peopleList"))
+    }
+
+    var html = "";
+
+    peopleList.forEach(function (element , index){
+        html+="<tr>"
+        html+="<td>" + element.name + "</td>";
+        html+="<td>" + element.age + "</td>";
+        html+="<td>" + element.address + "</td>";
+        html+="<td>" + element.email + "</td>";
+        html+='<td><button onclick="deleteData('+index+')" class="btn btn-danger m-2">Delete</button><button onclick="updateData('+index+')" class="btn btn-warning">Edit</button></td>';
+        html+="</tr>"
+    });
+
+    document.querySelector("#crudTable tbody").innerHTML = html;
+
+}
+
+document.onload = showData();
+
+// function add to data
+
+function AddData(){
+    // if form is validate
+
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var address = document.getElementById("address").value;
+    var email = document.getElementById("email").value;
+
+    var peopleList;
+    if(localStorage.getItem("peopleList")==null){
+        peopleList = [];
+    }else{
+        peopleList= JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    peopleList.push({
+        name:name,
+        age:age,
+        address:address,
+        email:email,
+    });
+
+    localStorage.setItem("peopleList",JSON.stringify(peopleList))
+
+    showData();
+
+    document.getElementById("name").value
+}
