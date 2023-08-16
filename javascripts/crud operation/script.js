d02ca5bc76ebe90229ed3ea14666739e5fdc78e3
akashkhot12 <akashkhot03@gmail.com>
2023-08-16 14:24:13 +0530

    data value get

--- javascripts/crud operation/script.js
+++ javascripts/crud operation/script.js
@@ -3,4 +3,32 @@ function validForm() {
     var age = document.getElementById("age").value
     var address = document.getElementById("address").value
     var email = document.getElementById("email").value
+
+    if(name==""){
+        alert("name is required");
+        return false;
+    }
+
+    if(age==""){
+        alert("age is required");
+        return false;
+    }
+    else if(age<1){
+        alert('age must be positive number')
+        return false ; 
+    }
+    if(address==""){
+        alert("address is required");
+        return false;
+    }
+    if(email==""){
+        alert("email is required");
+        return false;
+    }
+    else if(!email.include("@")){
+        alert("Invalid email address");
+        return false;
+    }
+
+    return true;
 }
