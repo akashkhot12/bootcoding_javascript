9f9b23ad62e5a1589f06638bc84197fce37b396c
akashkhot12 <akashkhot03@gmail.com>
2023-08-19 10:34:54 +0530

    contain duplicates

---
+++ javascripts/leetcode/217ContainsDuplicate.js
@@ -0,0 +1,17 @@
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+let array = [1,2,3,1]
+for (let index = 0; index < array.length; index++) {
+    
+}
