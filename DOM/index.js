d8ff346ddc2a6773be2600fbe4ba35f263bca3b8
akashkhot12 <akashkhot03@gmail.com>
2023-08-24 12:25:53 +0530

    dom

--- DOM/index.js
+++ DOM/index.js
@@ -6,8 +6,20 @@ function content(){
 
 function mOver(obj) {
     obj.innerHTML = "I am here .."
+   
 }
 
 function mOut(obj) {
     obj.innerHTML = "the mouse is removed"
 }
+
+let btn = document.querySelector('.btn');
+let heading = document.querySelector('.heading');
+
+btn.addEventListener('click',function(){
+    heading.innerHTML = "I am the heading"
+})
+
+heading.addEventListener('mouseover',()=>{
+    heading.style.fontSize = "100px"
+})
