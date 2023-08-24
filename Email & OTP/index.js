164c61c6865afa49df9e1da4528cc39a8026a380
akashkhot12 <akashkhot03@gmail.com>
2023-08-24 11:00:41 +0530

    adding m ail

--- Email & OTP/index.js
+++ Email & OTP/index.js
@@ -1,3 +1,6 @@
 function sendMail() {
-    
+    var userEmail = document.getElementById("userEmail").value;
+    var subjectLine = document.getElementById("subject").value;
+    var message = document.getElementById("body").value;
+    window.location.href = "mailto:"+userEmail+"?subject="+subjectLine+"&body="+message;
 }
