50a28be06f80f7dcb6a9d853ee5c33cc2f88bee1
akashkhot12 <akashkhot03@gmail.com>
2023-08-24 12:08:16 +0530

    mouseover event

--- DOM/index.js
+++ DOM/index.js
@@ -3,3 +3,11 @@
 function content(){
     document.getElementById("heading").innerHTML = "akash khot"
 }
+
+function mOver(obj) {
+    obj.innerHTML = "I am here .."
+}
+
+function mOut(obj) {
+    obj.innerHTML = "the mouse is removed"
+}
