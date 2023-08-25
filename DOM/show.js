4cf8e17bef9ee4aacad862ef781ed03336b618d2
akashkhot12 <akashkhot03@gmail.com>
2023-08-25 11:18:20 +0530

    show less or more data by using btn

--- DOM/show.js
+++ DOM/show.js
@@ -1,9 +1,18 @@
-para = document.getElementById("less");
-addPara = document.getElementById("more");
-btn = document.getElementById("btn");
+let lessData = document.getElementById("less");
+let moreData = document.getElementById("more");
+let btn  = document.getElementById("btn");
 
-function buttonState(){
-    if () {
-        
-    }
+ function btnState(){
+    btn.addEventListener("click",()=>{
+        if(btn.innerHTML == "more"){
+            btn.innerHTML = "less";
+            moreData.style.display = "inline"
+        }
+        else if (btn.innerHTML = "less"){
+            btn.innerHTML = "more";
+            moreData.style.display = "none"
+        }
+    })
 }
+
+btnState();
