7eba8c3d7c03d19980477bc1550ea0bffa830a4a
akashkhot12 <akashkhot03@gmail.com>
2023-08-25 10:47:54 +0530

    adding functions

---
+++ DOM/show.js
@@ -0,0 +1,9 @@
+para = document.getElementById("less");
+addPara = document.getElementById("more");
+btn = document.getElementById("btn");
+
+function buttonState(){
+    if () {
+        
+    }
+}
