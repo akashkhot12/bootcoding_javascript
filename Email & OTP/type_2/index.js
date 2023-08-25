f50eec45393d2e94995f43f02a58f81e9c069c29
akashkhot12 <akashkhot03@gmail.com>
2023-08-25 10:18:28 +0530

    index file

--- Email & OTP/type_2/index.js
+++ Email & OTP/type_2/index.js
@@ -5,10 +5,10 @@ function sendEmail(e){
         Host : "smtp.elasticemail.com",
         Username : "akashkhot03@gmail.com",
         Password : "AABA6C3BD87F571D395199AC06B3DC4936F6",
-        To : "chetana27Bhanarkar@getMaxListeners.com",
-        From : document.getElementById("userEmail").value,
+        To : document.getElementById("userEmail").value,
+        From : 'akashkhot03@gmail.com',
         Subject : document.getElementById("subject").value,
-        Body : document.getElementById("body").value
+        Body : document.getElementById("body").value,
     }).then(
       message => alert(message)
     );
