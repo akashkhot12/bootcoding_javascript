51f16c3adf1b5d29c9086cd53a03a50a4e5b1bfc
akashkhot12 <akashkhot03@gmail.com>
2023-08-26 12:19:08 +0530

    SENDING MAIL TO USER

--- Email & OTP/task1/index.js
+++ Email & OTP/task1/index.js
@@ -1,2 +1,23 @@
 
 
+ function sendEmail(e){
+    e.preventDefault();
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "akashkhot03@gmail.com",
+        Password : "AABA6C3BD87F571D395199AC06B3DC4936F6",
+        To : document.getElementById("userEmail").value,
+        From : 'akashkhot03@gmail.com',
+        Subject : document.getElementById("subject").value,
+        Body : document.getElementById("body").value,
+    }).then(
+      message => {
+        if (message==="OK") {
+          alert("your mail was sent on "+document.getElementById("userEmail").value);
+        }
+      }
+    );
+    
+}
+
+
