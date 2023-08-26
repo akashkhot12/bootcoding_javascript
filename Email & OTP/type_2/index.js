720f4779a97edd0ba8370bb2cf9d1dadbf98139b
akashkhot12 <akashkhot03@gmail.com>
2023-08-26 15:10:12 +0530

    complete verification code

--- Email & OTP/type_2/index.js
+++ Email & OTP/type_2/index.js
@@ -6,6 +6,26 @@ function sentOTP() {
   let emailBody = `<h2>Your OTP is </h2><h1>${OTP}</h1>`
   return emailBody;
 }
+
+function trueFalse(){
+  const btnIpt = document.getElementById("btnIpt");
+  const btnOTP = document.getElementById("btnOTP");
+  btnOTP.addEventListener('click', () => {
+    if (btnIpt.value == OTP) {
+      let result = document.getElementById("result")
+      result.innerHTML = `<label for="exampleFormControlInput1" class="fs-1">*Email verified</label>`
+      result.style.color = "green"
+    }
+    else {
+      let result = document.getElementById("result")
+      result.innerHTML = `<label for="exampleFormControlInput1 class="fs-1">*Invalid Input</label>`
+      result.style.color = "red"
+    }
+  })
+}
+
+
+
 function sendEmail(e) {
   e.preventDefault();
   Email.send({
@@ -19,23 +39,9 @@ function sendEmail(e) {
   }).then(
     message => {
       if (message === "OK") {
-        result.innerHTML = `<label for="exampleFormControlInput1">OTP was send</label>`
+        result.innerHTML = `<label for="exampleFormControlInput1" class="fs-1">OTP send</label>`
         result.style.color = "black"
-        const btnIpt = document.getElementById("btnIpt");
-        const btnOTP = document.getElementById("btnOTP");
-
-        btnOTP.addEventListener('click', () => {
-          if (btnIpt.value == OTP) {
-            let result = document.getElementById("result")
-            result.innerHTML = `<label for="exampleFormControlInput1">*Email verified</label>`
-            result.style.color = "green"
-          }
-          else {
-            let result = document.getElementById("result")
-            result.innerHTML = `<label for="exampleFormControlInput1">*Invalid Input</label>`
-            result.style.color = "red"
-          }
-        })
+        trueFalse()
       }
     }
   );
