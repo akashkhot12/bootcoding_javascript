671755f246e50189ecb4414cf7d2307521d71c17
akashkhot12 <akashkhot03@gmail.com>
2023-08-26 14:24:45 +0530

    otp sending by using mail

--- Email & OTP/task1/index.js
+++ Email & OTP/task1/index.js
@@ -1,3 +1,4 @@
+// sending mail
 
  function sendEmail(){
     Email.send({
@@ -12,6 +13,34 @@
       message =>alert(message)
     );
     
+}
+
+
+// sending OTP
+
+function sendingOTP(){;
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "akashkhot03@gmail.com",
+        Password : "AABA6C3BD87F571D395199AC06B3DC4936F6",
+        To : document.getElementById("userEmail").value,
+        From : 'akashkhot03@gmail.com',
+        Subject : document.getElementById("subject").value,
+        Body : sentOTP(),
+    }).then(
+      message => {
+        if (message==="OK") {
+          alert("your OTP was sent on "+document.getElementById("userEmail").value);
+        }
+      }
+    );
+    
+}
+
+function sentOTP(){
+   let OTP = Math.floor((Math.random()*1000000)+1);
+   let emailBody = `<h2>Your OTP is </h2><h1>${OTP}</h1>`
+   return emailBody;
 }
 
 
--- Email & OTP/type_2/index.js
+++ Email & OTP/type_2/index.js
@@ -1,30 +1,46 @@
 
-let otpVerify = document.querySelector(".otpVerify")
+let otpVerify = document.querySelector(".otpVerify")[0]
+let OTP ; 
+function sentOTP() {
+  OTP = Math.floor((Math.random() * 1000000) + 1);
+  let emailBody = `<h2>Your OTP is </h2><h1>${OTP}</h1>`
+  return emailBody;
+}
+function sendEmail(e) {
+  e.preventDefault();
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "akashkhot03@gmail.com",
+    Password: "AABA6C3BD87F571D395199AC06B3DC4936F6",
+    To: document.getElementById("userEmail").value,
+    From: 'akashkhot03@gmail.com',
+    Subject: document.getElementById("subject").value,
+    Body: sentOTP(),
+  }).then(
+    message => {
+      if (message === "OK") {
+        result.innerHTML = `<label for="exampleFormControlInput1">OTP was send</label>`
+        result.style.color = "black"
+        const btnIpt = document.getElementById("btnIpt");
+        const btnOTP = document.getElementById("btnOTP");
 
- function sendEmail(e){
-    e.preventDefault();
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "akashkhot03@gmail.com",
-        Password : "AABA6C3BD87F571D395199AC06B3DC4936F6",
-        To : document.getElementById("userEmail").value,
-        From : 'akashkhot03@gmail.com',
-        Subject : document.getElementById("subject").value,
-        Body : sentOTP(),
-    }).then(
-      message => {
-        if (message==="OK") {
-          alert("your OTP was sent on "+document.getElementById("userEmail").value);
-          otpVerify.style.display = "block";
-        }
+        btnOTP.addEventListener('click', () => {
+          if (btnIpt.value == OTP) {
+            let result = document.getElementById("result")
+            result.innerHTML = `<label for="exampleFormControlInput1">*Email verified</label>`
+            result.style.color = "green"
+          }
+          else {
+            let result = document.getElementById("result")
+            result.innerHTML = `<label for="exampleFormControlInput1">*Invalid Input</label>`
+            result.style.color = "red"
+          }
+        })
       }
-    );
-    
+    }
+  );
+
 }
 
-function sentOTP(){
-   let OTP = Math.floor((Math.random()*1000000)+1);
-   let emailBody = `<h2>Your OTP is </h2><h1>${OTP}</h1>`
-   return emailBody;
-}
+
 
