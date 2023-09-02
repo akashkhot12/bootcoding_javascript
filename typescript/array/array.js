6e0e9d66a250d3186670c132ddf572e3260fc174
akashkhot12 <akashkhot03@gmail.com>
2023-09-02 13:49:40 +0530

    using splits methods

--- typescript/array/array.js
+++ typescript/array/array.js
@@ -2,6 +2,7 @@ var fruits = ['Apple', 'Orange', 'Banana'];
 for (var index in fruits) {
     console.log(fruits[index]); // output: Apple Orange Banana
 }
+
 for (var i = 0; i < fruits.length; i++) {
     console.log(fruits[i]); // output: Apple Orange Banana
 }
