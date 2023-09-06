4b344ccf5c440e9e93bed7a4f5c4da3c39ded08e
akashkhot12 <akashkhot03@gmail.com>
2023-09-06 12:53:02 +0530

    server index

--- node Js/server.js
+++ node Js/server.js
@@ -2,7 +2,7 @@ let http = require('http');
 
 http.createServer((req,res)=>{
     // console.log('welcome akash');
-    res.write('<h1>hello</h1>');
+    res.write('<h1>hello world</h1>');
     res.end();
 }).listen(5051);
 
