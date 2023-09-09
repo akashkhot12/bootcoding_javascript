69b45ecf247416f76df35dc7b88ce526bf234e3c
akashkhot12 <akashkhot03@gmail.com>
2023-09-09 11:24:35 +0530

    created a file by using for loop

---
+++ node_Js/createdFile.js
@@ -0,0 +1,13 @@
+// file created code 
+
+// const fs  = require ('fs');
+// const path = require('path');
+// const dirPath=path.join(__dirname,'files')
+// for (let index = 0; index < 5; index++) {
+
+//     fs.writeFileSync(dirPath+"/hello"+index+".txt","a simple text")
+// }
+
+
+
+// files read code 
