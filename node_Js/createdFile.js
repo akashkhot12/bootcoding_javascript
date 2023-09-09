86ada58a6cdde68f66e75c39539bb3e782c62e3a
akashkhot12 <akashkhot03@gmail.com>
2023-09-09 11:27:11 +0530

    read files

--- node_Js/createdFile.js
+++ node_Js/createdFile.js
@@ -1,8 +1,8 @@
 // file created code 
 
-// const fs  = require ('fs');
-// const path = require('path');
-// const dirPath=path.join(__dirname,'files')
+const fs  = require ('fs');
+const path = require('path');
+const dirPath=path.join(__dirname,'files')
 // for (let index = 0; index < 5; index++) {
 
 //     fs.writeFileSync(dirPath+"/hello"+index+".txt","a simple text")
@@ -11,3 +11,8 @@
 
 
 // files read code 
+fs.readdir(dirPath,(err,files)=>{
+    files.forEach((item)=>{
+        console.log("your file is => "+ item);
+    })
+})
