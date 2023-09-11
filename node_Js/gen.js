ceeab715e465a6823acb4b96952a9aa6e6d5f6e3
akashkhot12 <akashkhot03@gmail.com>
2023-09-11 11:57:47 +0530

    read file

--- node_Js/gen.js
+++ node_Js/gen.js
@@ -1,5 +1,10 @@
+const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
 const dirPath = path.join(__dirname,'crud');
 const filePath = `${dirPath}/data.txt`
-fs.writeFileSync(filePath,'this is a simple text file');
+// fs.writeFileSync(filePath,'this is a simple text file');  => create
+
+fs.readFile(filePath,'utf8',(err,item)=>{
+    console.log(item);
+}) 
