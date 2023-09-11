1c90218f83f4e4a6ce173eaba0fe498e009ed3f0
akashkhot12 <akashkhot03@gmail.com>
2023-09-11 11:54:54 +0530

    create file from terminal

--- node_Js/gen.js
+++ node_Js/gen.js
@@ -1,2 +1,5 @@
 const fs = require('fs');
-const path = require('./crud');
+const path = require('path');
+const dirPath = path.join(__dirname,'crud');
+const filePath = `${dirPath}/data.txt`
+fs.writeFileSync(filePath,'this is a simple text file');
