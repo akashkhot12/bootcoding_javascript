0a142ec793e4287c1e21ded80defd337d698339a
akashkhot12 <akashkhot03@gmail.com>
2023-09-12 12:49:38 +0530

    file generete

--- node_Js/sycAsync.js
+++ node_Js/sycAsync.js
@@ -1,5 +1,11 @@
-console.log("login");
-setTimeout(()=>{
-    console.log("waiting");
-},2000)
-console.log("logout");
+// console.log("login");
+
+// setTimeout(()=>{
+//     console.log("waiting");
+// },2000)
+
+// console.log("logout");
+
+
+const fs = require('fs');
+fs.writeFileSync('megafile.txt','this is mega file')
