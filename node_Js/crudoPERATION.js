73517f2432991357c46b1e1e895f8522e3e92abf
akashkhot12 <akashkhot03@gmail.com>
2023-09-12 10:46:11 +0530

    crud  operation using node js

---
+++ node_Js/crudoPERATION.js
@@ -0,0 +1,29 @@
+const { log } = require('console');
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname,'crud');
+const filePath = `${dirPath}/data.txt`
+fs.writeFileSync(filePath,'this is a simple text file');  
+
+
+
+fs.readFile(filePath,'utf8',(err,item)=>{
+    console.log(item);
+})  
+
+
+
+// fs.appendFile(filePath,' and file name is data.txt' ,(err)=>{
+//   if(!err) console.log("file is updated");  
+// })  
+
+
+
+// fs.rename(filePath,`${dirPath}/dataCreate.txt`,(err)=>{
+//     if(!err) console.log("file is renamed");
+// }) 
+
+
+
+
+// fs.unlinkSync(`${dirPath}/dataCreate.txt`) 
