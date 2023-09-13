7fe61fe178b094265882dba8de6652a90ee0876a
akashkhot12 <akashkhot03@gmail.com>
2023-09-13 11:49:17 +0530

    create server using node js

--- node_Js/express.js
+++ node_Js/express.js
@@ -1,2 +1,16 @@
 const express = require ('express');
 const app = express();
+
+app.get('' , (req , res)=>{
+
+   res.send('hello from home page')
+
+})
+
+app.get('/serverPage' , (req , res)=>{
+
+    res.send('hello from simple server page')
+ 
+ });
+
+ app.listen(5000)
---
+++ node_Js/express1.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app=express();
+
+app.get('',(req,res)=>{
+    res.send("hello this is web page")
+})
+
+app.get('/about',(req,res)=>{
+    res.send("<h1>lorem</h1>")
+})
+app.listen(3100)
