14aecad66d220f789c1e7bc77e29741d9c44e000
akashkhot12 <akashkhot03@gmail.com>
2023-09-13 10:17:21 +0530

    sync async

--- node_Js/sycAsync.js
+++ node_Js/sycAsync.js
@@ -1,11 +1,11 @@
-// console.log("login");
+console.log("login");
 
-// setTimeout(()=>{
-//     console.log("waiting");
-// },2000)
+setTimeout(()=>{
+    console.log("waiting");
+},1000)
 
-// console.log("logout");
+console.log("logout");
 
 
-const fs = require('fs');
-fs.writeFileSync('megafile.txt','this is mega file')
+// const fs = require('fs');
+// fs.writeFileSync('megafile.txt','this is mega file')
