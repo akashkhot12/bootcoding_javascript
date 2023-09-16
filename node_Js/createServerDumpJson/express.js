3eccb46fb4a04522b287c8accbf7a53821de4feb
akashkhot12 <akashkhot03@gmail.com>
2023-09-16 20:06:20 +0530

    link with pages

---
+++ node_Js/createServerDumpJson/express.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("",(req,res)=>{
+    res.send("this is json data")
+})
+
+app.listen(4000);
--- node_Js/express1.js
+++ node_Js/express1.js
@@ -2,13 +2,13 @@ const express = require('express')
 
 const app=express();
 
-app.get('',(req,res)=>{
+app.get('/welcome',(req,res)=>{
     console.log("name is => " ,req.query.name);
-    res.send("welcome "+req.query.name)
+    res.send("welcome "+req.query.name + ` <a href = /about> go to about page</a>`)
 })
 
 app.get('/about',(req,res)=>{
-    res.send("<h1>lorem</h1>")
+    res.send(`<h1>lorem</h1> <a href ="/welcome"> go to welcome page</a>`)
 })
 app.listen(3100)
 
