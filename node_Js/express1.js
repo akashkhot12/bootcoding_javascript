557e1e668c5c490721ed21940c841a262777602d
akashkhot12 <akashkhot03@gmail.com>
2023-09-16 20:31:15 +0530

    express with html file show

--- node_Js/express1.js
+++ node_Js/express1.js
@@ -1,15 +1,27 @@
-const express = require('express')
+// const express = require('express')
 
-const app=express();
+// const app=express();
 
-app.get('/',(req,res)=>{
-    console.log("name is => " ,req.query.name);
-    res.send(`hello ${req.query.name} <a href = /about> go to about page</a>`)
-})
+// app.get('/',(req,res)=>{
+//     console.log("name is => " ,req.query.name);
+//     res.send(`hello ${req.query.name} <a href = /about> go to about page</a>`)
+// })
 
-app.get('/about',(req,res)=>{
-    res.send(`<h1>lorem</h1> <a href ="/welcome"> go to welcome page</a>`)
-})
-app.listen(3100)
+// app.get('/about',(req,res)=>{
+//     res.send(`<h1>lorem</h1> <a href ="/welcome"> go to welcome page</a>`)
+// })
+// app.listen(3100)
 
+
+const express = require ("express");
+ const path  = require("path");
+
+const app = express();
+
+const pathData  = path.join(__dirname,'htmlWithExpress');
+console.log(pathData);
+
+app.use(express.static(pathData))
+
+app.listen(3300)
 
