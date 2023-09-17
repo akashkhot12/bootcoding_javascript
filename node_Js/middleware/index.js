716a4552c76ef288a188b3b492f14def5cbe6628
akashkhot12 <akashkhot03@gmail.com>
2023-09-17 10:19:39 +0530

    adding about page

--- node_Js/middleware/index.js
+++ node_Js/middleware/index.js
@@ -5,4 +5,8 @@ const app = express();
 app.get('',(req,res)=>{
     res.send("welcome the home page")
 })
+
+app.get('/about',(req,res)=>{
+    res.send("welcome the about page")
+})
 app.listen(4100)
