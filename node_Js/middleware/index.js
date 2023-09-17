02e5372fac4b3e0971530e40fef7d8798907e722
akashkhot12 <akashkhot03@gmail.com>
2023-09-17 11:00:08 +0530

    changes

--- node_Js/middleware/index.js
+++ node_Js/middleware/index.js
@@ -2,18 +2,18 @@ const express = require("express");
 
 const app = express();
 
-const reqFilter=(req,res,next)=>{
-    console.log("reqFilter");
-    next();
-}
+const reqFilter = (req, res, next) => {
+  console.log("reqFilter");
+  next();
+};
 
 app.use(reqFilter);
 
-app.get('',(req,res)=>{
-    res.send("welcome the home page")
-})
+app.get("", (req, res) => {
+  res.send("welcome the home page");
+});
 
-app.get('/about',(req,res)=>{
-    res.send("welcome the about page")
-})
-app.listen(4100)
+app.get("/about", (req, res) => {
+  res.send("welcome the about page");
+});
+app.listen(4100);
