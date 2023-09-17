293e9aacf3ae768be8b70f6456155b50a35e11bd
akashkhot12 <akashkhot03@gmail.com>
2023-09-17 11:04:06 +0530

    if ales statement

--- node_Js/middleware/index.js
+++ node_Js/middleware/index.js
@@ -3,8 +3,16 @@ const express = require("express");
 const app = express();
 
 const reqFilter = (req, res, next) => {
-  console.log("reqFilter");
-  next();
+  if(!req.query.age){
+    res.send("please provide your age")
+  }
+
+  else if(req.query.age<18){
+    res.send("you are under age")
+  }
+  else{
+    next();
+  }
 };
 
 app.use(reqFilter);
