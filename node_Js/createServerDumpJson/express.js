c21dcf0a72acdb6ad3e430a0ebb93bece605b74d
akashkhot12 <akashkhot03@gmail.com>
2023-09-20 13:14:42 +0530

    learning express js

--- node_Js/createServerDumpJson/express.js
+++ node_Js/createServerDumpJson/express.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("",(req,res)=>{
-    res.send("this is json data")
-})
+app.get("", (req, res) => {
+  res.send("this is json data");
+});
 
 app.listen(4000);
