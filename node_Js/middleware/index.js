d6cac80d1ddc373992bb4e97c4f166147119926a
akashkhot12 <akashkhot03@gmail.com>
2023-09-20 13:13:45 +0530

    indexfile

--- node_Js/middleware/index.js
+++ node_Js/middleware/index.js
@@ -3,14 +3,11 @@ const express = require("express");
 const app = express();
 
 const reqFilter = (req, res, next) => {
-  if(!req.query.age){
-    res.send("please provide your age")
-  }
-
-  else if(req.query.age<18){
-    res.send("you are under age")
-  }
-  else{
+  if (!req.query.age) {
+    res.send("please provide your age");
+  } else if (req.query.age < 18) {
+    res.send("you are under age");
+  } else {
     next();
   }
 };
