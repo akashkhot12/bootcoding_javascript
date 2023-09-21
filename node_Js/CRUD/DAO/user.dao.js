c28129f7bae79e66a914975bbef53b2dd82659e1
akashkhot12 <akashkhot03@gmail.com>
2023-09-21 11:08:15 +0530

    create table qqquery

---
+++ node_Js/CRUD/DAO/user.dao.js
@@ -0,0 +1,30 @@
+const {Pool,Client,Query} = require('pg');
+const db = require('../operation1/connections');
+
+const query = async(req,res)=>{
+    const pool = new Pool(db.data);
+    const [result] = pool.query(sql,(err,res)=>{
+        if(err){
+            throw err;
+        }
+        return result;
+    })
+}
+
+const genTable = async()=>{
+    const pool =new Pool(db.data);
+    const table = `CREATE TABLE IF NOT EXISTS Public.Mania (
+        Id 	INT PRIMARY KEY,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        gender CHAR(1),
+        birthdate DATE,
+        email_id VARCHAR(100) UNIQUE,
+        country_of_birth VARCHAR(50)
+    );`
+
+    const result = pool.query(table);
+    pool.end();
+}
+
+genTable();
