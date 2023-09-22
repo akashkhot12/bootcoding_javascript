93261003cb73e06ff4e73958c0d29a0ba83ef4ee
akashkhot12 <akashkhot03@gmail.com>
2023-09-22 13:16:43 +0530

    fregenret api

--- node_Js/CRUD/api/data.api.js
+++ node_Js/CRUD/api/data.api.js
@@ -3,7 +3,7 @@ const app = express();
 const router  = express.Router();
 const dataservice = require("../service/api.service")
 
-router.get('best',async(req,res)=>{
+router.get('/best',async(req,res)=>{
     let dbResponse = await dataservice.getService();
     console.log(dbResponse);
     res.status(200).json(dbResponse)
--- node_Js/CRUD/index.js
+++ node_Js/CRUD/index.js
@@ -6,3 +6,6 @@ app.use(express.json());
 app.use('/show',data).listen(5000,()=>{
     console.log('your code is run !');
 });
+
+// app.use(express.json())
+// app.use('/gif',data).listen(8000)
