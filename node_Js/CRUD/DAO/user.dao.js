97e636d1bd9130cc00f876f7c5e394cfbf681c8a
akashkhot12 <akashkhot03@gmail.com>
2023-09-22 12:44:46 +0530

    getting data form db

--- node_Js/CRUD/DAO/user.dao.js
+++ node_Js/CRUD/DAO/user.dao.js
@@ -1,2 +1,18 @@
 const{Pool,Client,Query}=require("pg");
-const db = require("../connections/database")
+const db = require("../connections/database");
+
+const getData = async()=>{
+    const pool = new Pool(db.dbcongif);
+    const qr = `select * from public.person`
+    const res = await pool.query(qr);
+    if (res.affectedRows) {
+        console.log(res.rows);
+    }
+    pool.end();
+    console.log(res.rows);
+    return res.rows;
+}
+
+getData();
+
+module.exports = { getData}
