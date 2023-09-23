0f93a5d796e7ee3bb346c87ab82135a7d6c0321e
akashkhot12 <akashkhot03@gmail.com>
2023-09-23 10:52:00 +0530

    dump data

--- node_Js/nodeCrud2/DAO/data.dao.js
+++ node_Js/nodeCrud2/DAO/data.dao.js
@@ -1,2 +1,18 @@
 const {Pool,Client,Query} = require('pg')
 const path = require('../database/connection.db');
+
+const getData = async()=>{
+const pool = new Pool(path.databse);
+const querry = `select * from public.person`
+const response = pool.query(querry);
+if (response.affectedRows) {
+    console.log(response.rows);
+}
+pool.end();
+console.log(response.rows);
+return response.rows;
+}
+
+getData();
+
+module.exports = {getData}
---
+++ node_Js/nodeCrud2/database/connection.db.js
@@ -0,0 +1,7 @@
+const databse = {
+    user :'postgres',
+    host : 'localhost',
+    databse : 'node',
+    password : 'Akash3975'
+}
+module.exports={databse}
