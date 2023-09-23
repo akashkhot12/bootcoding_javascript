8f7298b09dd3315baf74e8051ba301a43860eddd
akashkhot12 <akashkhot03@gmail.com>
2023-09-23 09:57:06 +0530

    export the router

--- node_Js/nodeCrud2/API/get.api.js
+++ node_Js/nodeCrud2/API/get.api.js
@@ -2,3 +2,12 @@ const express =  require ('express');
 const app= express();
 const router = express.Router();
 const dataservice  = require('../service/get.service')
+
+
+router.get('userData',async(req,res)=>{
+    let database = await dataservice.getService();
+    console.log(database);
+    res.status(200).json(database)
+});
+
+module.exports = router
