e29457bdc4c9d86742d21d9a1e9c9b526466559e
akashkhot12 <akashkhot03@gmail.com>
2023-09-23 09:49:51 +0530

    create data for relaive path

--- node_Js/nodeCrud2/index.js
+++ node_Js/nodeCrud2/index.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
-const data = ('../')
+ const data = require('./API/get.api');
+
+ app.use(express.json());
+ app.use('/useApi',data).listen(5544,()=>{
+    console.log("your server is up");
+ })
