7c7a47ddf583d91920352b4da723ff87a7d6008a
akashkhot12 <akashkhot03@gmail.com>
2023-09-25 13:30:11 +0530

    gentret data

--- Express/DAO/data.dao.js
+++ Express/DAO/data.dao.js
@@ -1,6 +1,8 @@
 const { Pool, Client, Query } = require("bg");
 const db = require("../connections/dbConfig");
 
+
+
 const query = async (req, res) => {
   const pool = new Pool(db.dbConfig);
   const [result] = pool.query(sql, (err, res) => {
