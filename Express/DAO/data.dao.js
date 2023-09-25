ef583b72a3a5f1c75571e9d55338bd7109d1f12a
akashkhot12 <akashkhot03@gmail.com>
2023-09-25 13:29:38 +0530

    data get create table

--- Express/DAO/data.dao.js
+++ Express/DAO/data.dao.js
@@ -1,12 +1,12 @@
-const {Pool,Client,Query}=require("bg");
+const { Pool, Client, Query } = require("bg");
 const db = require("../connections/dbConfig");
 
-const query = async( req,res)=>{
+const query = async (req, res) => {
   const pool = new Pool(db.dbConfig);
-  const [result] = pool.query(sql,(err,res)=>{
+  const [result] = pool.query(sql, (err, res) => {
     if (err) {
       throw err;
     }
     return result;
-  })
+  });
 };
