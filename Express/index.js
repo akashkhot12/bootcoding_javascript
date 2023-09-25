fb49a069b3727194cf5cccc1eb42115c18697f25
akashkhot12 <akashkhot03@gmail.com>
2023-09-25 12:26:16 +0530

    genretd api

--- Express/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const data = require("./Api/data.api");
-
-app.use(express.json());
-app.use("/data", data).listen(2000, () => {
-  console.log("server is up");
-});
--- Express/service/data.service.js
+++
@@ -1,7 +0,0 @@
-const data = require("../DAO/data.dao");
-
-async function insertService(id, name, age, address, salary) {
-  return await data.insdata(id, name, age, address, salary);
-}
-
-module.exports = { insertService };
