efcf308568e19f603651d799ec0ce82962718fe0
akashkhot12 <akashkhot03@gmail.com>
2023-09-25 13:30:35 +0530

    formated data

--- Express/DAO/data.dao.js
+++ Express/DAO/data.dao.js
@@ -1,8 +1,6 @@
 const { Pool, Client, Query } = require("bg");
 const db = require("../connections/dbConfig");
 
-
-
 const query = async (req, res) => {
   const pool = new Pool(db.dbConfig);
   const [result] = pool.query(sql, (err, res) => {
