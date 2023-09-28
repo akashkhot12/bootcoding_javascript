fe481db7c73487009a745c91789f0f27080ae0eb
akashkhot12 <akashkhot03@gmail.com>
2023-09-28 16:03:42 +0530

    fix all problems

--- typescript/array/array.js
+++ typescript/array/array.js
@@ -1,8 +1,8 @@
-var fruits = ['Apple', 'Orange', 'Banana'];
+var fruits = ["Apple", "Orange", "Banana"];
 for (var index in fruits) {
-    console.log(fruits[index]); // output: Apple Orange Banana
+  console.log(fruits[index]); // output: Apple Orange Banana
 }
 
 for (var i = 0; i < fruits.length; i++) {
-    console.log(fruits[i]); // output: Apple Orange Banana
+  console.log(fruits[i]); // output: Apple Orange Banana
 }
