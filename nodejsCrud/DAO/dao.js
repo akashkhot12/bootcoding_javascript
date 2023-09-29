69f4c02a95cd29feeccf24ad75b68b5b06954f89
akashkhot12 <akashkhot03@gmail.com>
2023-09-29 12:11:51 +0530

    create table

--- nodejsCrud/DAO/dao.js
+++ nodejsCrud/DAO/dao.js
@@ -13,7 +13,7 @@ const query = async(req,res)=>{
 
 const createTable = async()=>{
     const pool = new Pool(db.database);
-    const genTable = `CREATE TABLE IF NOT EXIST Employee  
+    const genTable = `CREATE TABLE IF NOT EXIST public.Employee  
     (  
     EmployeeID int PRIMARY KEY,  
     FirstName varchar(255),  
@@ -27,3 +27,17 @@ const createTable = async()=>{
 }
 
 createTable();
+
+const insertData = async(EmployeeID,FirstName,LastName,Email,AddressLine,City)=>{
+    const pool = new Pool(db.database);
+    const insert = `insert into public.Employee(EmployeeID,FirstName,LastName,Email,AddressLine,City) values('${EmployeeID}','${FirstName}','${LastName}','${Email}','${AddressLine}','${City}')`;
+    const res = await pool.query(insert);
+    let message = "data is update";
+    if (res.affectedRows) {
+        message :message;
+    }
+    pool.end();
+    return res;
+}
+
+insertData(1,'vansh','belekar','vanshbe@gmail.com','khat road bhandara','bhandara')
