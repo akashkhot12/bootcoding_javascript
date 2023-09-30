cfe7a593ba09e3aa2185e7acf7704c3e08f92692
akashkhot12 <akashkhot03@gmail.com>
2023-09-30 13:02:58 +0530

    adding service

--- nodejsCrud/service/service2.js
+++ nodejsCrud/service/service2.js
@@ -1,4 +1,11 @@
-const data  = require("../DAO/dao2");
+const data = require("../DAO/dao2");
+
+async function insertService(name, address, email, phone) {
+  return await data.insertdata(name, address, email, phone);
+}
 
 
-async insertService
+// insertService("pallavi","hudkeshwar","pallavi@gmail.com","9884787828")
+
+
+module.exports = {insertService}
