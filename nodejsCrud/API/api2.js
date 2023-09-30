a1046c831c871f0d2f72a83ce9b275a68b3001a6
akashkhot12 <akashkhot03@gmail.com>
2023-09-30 13:25:23 +0530

    created api

--- nodejsCrud/API/api2.js
+++ nodejsCrud/API/api2.js
@@ -1,4 +1,15 @@
-const express= require ("express");
+const express = require("express");
 const app = express();
 const router = express.Router();
-const db = require("../service/service2")
+const db = require("../service/service2");
+
+router.post("/insert", async (req, res) => {
+  let name = req.body.name;
+  let address = req.body.address;
+  let email = req.body.email;
+  let phone = req.body.phone;
+
+  let dbResponse = await db.insertService(name, address, email, phone);
+  console.log("data inserted", dbResponse);
+  req.status(200).json({ staus: 200, message: dbResponse });
+});
