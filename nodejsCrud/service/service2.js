14216a866908f1f10af88e2f52781e58220dbb77
akashkhot12 <akashkhot03@gmail.com>
2023-10-01 13:29:40 +0530

    create service for get method

--- nodejsCrud/service/service2.js
+++ nodejsCrud/service/service2.js
@@ -4,11 +4,10 @@ async function insertService(name, address, email, phone) {
   return await data.insertdata(name, address, email, phone);
 }
 
-
 // insertService("pallavi","hudkeshwar","pallavi@gmail.com","9884787828")
 
-async function getService(id){
-  return await data.getData(id)
+async function getService(id) {
+  return await data.getData(id);
 }
 
-module.exports = {insertService,getService}
+module.exports = { insertService, getService };
