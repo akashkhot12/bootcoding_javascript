a234f8a05e81c027e301b5d4da0b1b3d628596fd
akashkhot12 <akashkhot03@gmail.com>
2023-10-03 12:34:27 +0530

    filled column

--- nodejsCrud/service/service2.js
+++ nodejsCrud/service/service2.js
@@ -4,6 +4,7 @@ async function insertService(name, address, email, phone) {
   return await data.insertdata(name, address, email, phone);
 }
 
+
 // insertService("pallavi","hudkeshwar","pallavi@gmail.com","9884787828")
 
 async function getService(id) {
