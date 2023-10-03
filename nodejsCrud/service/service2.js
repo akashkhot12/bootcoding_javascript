b2d5c3ec6a4817802a6db4a766361c205f40648f
akashkhot12 <akashkhot03@gmail.com>
2023-10-03 12:35:10 +0530

    service file

--- nodejsCrud/service/service2.js
+++ nodejsCrud/service/service2.js
@@ -3,8 +3,6 @@ const data = require("../DAO/dao2");
 async function insertService(name, address, email, phone) {
   return await data.insertdata(name, address, email, phone);
 }
-
-
 // insertService("pallavi","hudkeshwar","pallavi@gmail.com","9884787828")
 
 async function getService(id) {
