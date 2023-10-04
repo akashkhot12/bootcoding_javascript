d4c34d771c597e9b3373b545f854e94f346c8b5e
akashkhot12 <akashkhot03@gmail.com>
2023-10-04 23:29:36 +0530

    db jason

--- nodejsCrud/API/api2.js
+++ nodejsCrud/API/api2.js
@@ -8,7 +8,6 @@ router.post("/insert", async (req, res) => {
   let address = req.body.address;
   let email = req.body.email;
   let phone = req.body.phone;
-
   let dbResponse = await db.insertService(name, address, email, phone);
   console.log("data inserted", dbResponse);
   req.status(200).json({ staus: 200, message: dbResponse });
