1dae3b92b4543a53ee16d487c847f1b77cbc06e7
akashkhot12 <akashkhot03@gmail.com>
2023-10-04 23:30:22 +0530

    index file

--- nodejsCrud/index2.js
+++ nodejsCrud/index2.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const data = require("./API/api2");
 
+
 app.use(express.json());
 app.use("/data1", data).listen(4000, () => {
   console.log("server is up");
