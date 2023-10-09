09cc3bc3a3beeac481850f21ea7a28a42cdc1785
akashkhot12 <akashkhot03@gmail.com>
2023-10-09 13:29:23 +0530

    insert data removed

--- nodejsCrud/service/service2.js
+++ nodejsCrud/service/service2.js
@@ -3,7 +3,7 @@ const data = require("../DAO/dao2");
 async function insertService(name, address, email, phone) {
   return await data.insertdata(name, address, email, phone);
 }
-insertService("pallavi","hudkeshwar","pallavi@gmail.com","9884787828")
+// insertService("pallavi","hudkeshwar","pallavi@gmail.com","9884787828")
 
 async function getService(id) {
   return await data.getData(id);
