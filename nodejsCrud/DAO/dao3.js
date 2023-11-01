fd606855fa2f721946e686b9bf748d3390a0e1eb
akashkhot12 <akashkhot03@gmail.com>
2023-11-01 15:28:57 +0530

    fromated documents

--- nodejsCrud/DAO/dao3.js
+++ nodejsCrud/DAO/dao3.js
@@ -1,14 +1,12 @@
-const {Pool,Client,Query}=require("pg");
+const { Pool, Client, Query } = require("pg");
 const db = require("../Connections/connection3");
 
-const query = async(req,res)=>{
+const query = async (req, res) => {
   const pool = new Pool(db.database);
-  const result = pool.query(sql,(err,res)=>{
+  const result = pool.query(sql, (err, res) => {
     if (err) {
-      throw err
+      throw err;
     }
     return result;
-  })
-}
-
-
+  });
+};
