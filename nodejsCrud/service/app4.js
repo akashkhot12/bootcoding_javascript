7f2bbb6d5b3651d90822136b1886846edeb46f41
akashkhot12 <akashkhot03@gmail.com>
2023-11-03 23:47:05 +0530

    created service

---
+++ nodejsCrud/service/app4.js
@@ -0,0 +1,19 @@
+const data = require("../DAO/app4");
+
+async function insertService(
+    teacherID,
+    firstName,
+    lastName,
+    batch,
+    sallary,
+    subject
+){
+    return await data.insertData(
+        teacherID,
+    firstName,
+    lastName,
+    batch,
+    sallary,
+    subject
+    )
+};
