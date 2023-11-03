8b7b0c56c3bfe94bd12b1a9ec8b832416aae4531
akashkhot12 <akashkhot03@gmail.com>
2023-11-03 14:38:34 +0530

    create query

--- nodejsCrud/DAO/app3.js
+++ nodejsCrud/DAO/app3.js
@@ -1,2 +1,12 @@
 const{Pool,Client,Query}=require("pg");
 const db = require("../Connections/app3");
+
+const query = async(req,res)=>{
+    const pool = new Pool(db.database);
+    const result = pool.query(sql,(err,res)=>{
+        if (err) {
+            throw err
+        }
+        return result;
+    })
+}
