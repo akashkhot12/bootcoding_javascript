1f52d9cda79d6f20c76d27d15f5121577049e7fa
akashkhot12 <akashkhot03@gmail.com>
2023-11-03 23:45:11 +0530

    created service

---
+++ nodejsCrud/service/app2.js
@@ -0,0 +1,19 @@
+const data =require("../DAO/app2");
+
+async function insertService(
+    bookID,
+    bookname,
+    pages,
+    type,
+    price,
+    typeofpages
+){
+ return await data.insertData(
+    bookID,
+    bookname,
+    pages,
+    type,
+    price,
+    typeofpages
+ )
+}
---
+++ nodejsCrud/service/app3.js
@@ -0,0 +1,18 @@
+const data = require("../DAO/app3")
+
+async function insertService(
+    gadgetID,
+    gadgetname,
+    object,
+    type,
+    model
+){
+    return await data.insertData(
+    gadgetID,
+    gadgetname,
+    object,
+    type,
+    model
+    )
+    
+}
