f110e6a482382708d4882b07c9cd6f77a7d901e8
akashkhot12 <akashkhot03@gmail.com>
2023-11-03 14:30:14 +0530

    genrte functions

--- nodejsCrud/DAO/app1.js
+++ nodejsCrud/DAO/app1.js
@@ -1,2 +1,13 @@
 const { Pool, Client, Query } = require("pg");
 const db = require("../Connections/app1");
+
+
+const query = async(res,res)=>{
+    const pool = new Pool(db.database);
+    const result = pool.query(sql,(err,res)=>{
+        if (err) {
+            throw err;
+        }
+        return result;
+    })
+}
