8fbe3950d108932979e8926b9382d2a00fb3a5b8
akashkhot12 <akashkhot03@gmail.com>
2023-11-04 22:47:44 +0530

    documents

--- nodejsCrud/service/app2.js
+++ nodejsCrud/service/app2.js
@@ -1,21 +1,21 @@
-const data =require("../DAO/app2");
+const data = require("../DAO/app2");
 
 async function insertService(
+  bookID,
+  bookname,
+  pages,
+  type,
+  price,
+  typeofpages
+) {
+  return await data.insertData(
     bookID,
     bookname,
     pages,
     type,
     price,
     typeofpages
-){
- return await data.insertData(
-    bookID,
-    bookname,
-    pages,
-    type,
-    price,
-    typeofpages
- )
+  );
 }
 
-module.exports={insertService}
+module.exports = { insertService };
