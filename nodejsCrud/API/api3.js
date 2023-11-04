fa7cde40b80703732f5493b9f1258242bd48b707
akashkhot12 <akashkhot03@gmail.com>
2023-11-04 10:25:08 +0530

    cfeating file

--- nodejsCrud/API/api3.js
+++ nodejsCrud/API/api3.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const app = express();
-const router =express.Router();
+const router = express.Router();
 const service = require("../service/service3");
 
+router.post("/insertData", async (req, res) => {
+  let StudentID = req.body.StudentID;
+  let FirstName = req.body.FirstName;
+  let LastName = req.body.LastName;
+  let Email = req.body.Email;
+  let AddressLine = req.body.AddressLine;
+  let City = req.body.City;
+
+  let dbResponse = await service.insertService(
+    StudentID,
+    FirstName,
+    LastName,
+    Email,
+    AddressLine,
+    City
+  );
+  console.log(dbResponse);
+  res.status(200).json({status:"success",message:dbResponse})
+});
+
+module.exports= router;
--- nodejsCrud/API/app1.js
+++ nodejsCrud/API/app1.js
@@ -2,3 +2,24 @@ const express = require("express");
 const app= express();
 const router = express.Router();
 const service = require("../service/app1");
+
+
+router.post("insertData",async(req,res)=>{
+    bagID,
+    bagname,
+    bagtype,
+    color,
+    price
+
+    let dbResponse = await service.insertService(
+        bagID,
+        bagname,
+        bagtype,
+        color,
+        price
+    ) 
+    console.log(dbResponse);
+    res.status(200).json({status:"success",message:dbResponse})
+})
+
+module.exports=router
